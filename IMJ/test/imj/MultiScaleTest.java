7ee0bb24c4a7e61526f81baa108f32a2fb681675
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-01-30 13:04:54 +0000

    [IMJ] Updated MultiscaleTest with adaptive variance analysis.

--- IMJ/test/imj/MultiScaleTest.java
+++ IMJ/test/imj/MultiScaleTest.java
@@ -13,15 +13,20 @@ import imj.IMJTools.StatisticsSelector;
 import imj.ImageOfBufferedImage.Feature;
 import imj.WatershedTest.WatershedStack;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageInputStream;
+
+import net.sourceforge.aprog.tools.MathTools.Statistics;
 
 import org.junit.Test;
 
@@ -30,7 +35,7 @@ import org.junit.Test;
  */
 public final class MultiScaleTest {
 	
-	@Test
+//	@Test
 	public final void test1() throws FileNotFoundException, IOException {
 		final TicToc timer = new TicToc();
 		final String imageId = "test/imj/12003.jpg";
@@ -73,6 +78,76 @@ public final class MultiScaleTest {
 		}
 		
 //		ImageComponent.showAdjusted(imageId, marker);
+	}
+	
+	@Test
+	public final void testVarianceSegmentation1() {
+		final TicToc timer = new TicToc();
+//		final String imageId = "test/imj/12003.jpg";
+		final String imageId = "../Libraries/images/16088-4.png";
+		
+		debugPrint("Loading image:", new Date(timer.tic()));
+		final Image image = image(imageId, Feature.MAX_RGB);
+		debugPrint("Done:", "time:", timer.toc(), "memory:", usedMemory());
+		
+		final int rowCount = image.getRowCount();
+		final int columnCount = image.getColumnCount();
+		final Image result = new ImageOfInts(rowCount, columnCount);
+		
+		debugPrint("Performing adaptive variance analysis:", new Date(timer.tic()));
+		final double threshold = 100.0;
+		final List<Rectangle> todo = new LinkedList<Rectangle>();
+		
+		todo.add(new Rectangle(0, 0, columnCount, rowCount));
+		
+		while (!todo.isEmpty()) {
+			final Rectangle r = todo.remove(0);
+			
+			if (r.isEmpty()) {
+				continue;
+			}
+			
+			final Statistics statistics = computeStatistics(image, r.y, r.x, r.y + r.height, r.x + r.width);
+			final double variance = statistics.getVariance();
+			
+			if (variance <= threshold) {
+				final int endY = r.y + r.height;
+				final int endX = r.x + r.width;
+				
+				for (int y = r.y; y < endY; ++y) {
+					for (int x = r.x; x < endX; ++x) {
+//						result.setValue(y, x, (int) statistics.getMean());
+						result.setFloatValue(y, x, (float) statistics.getVariance());
+					}
+				}
+			}
+			
+			if (threshold < variance) {
+				final int halfWidth = r.width / 2;
+				final int halfHeight = r.height / 2;
+				todo.add(new Rectangle(r.x, r.y, halfWidth, halfHeight));
+				todo.add(new Rectangle(r.x + halfWidth, r.y, r.width - halfWidth, halfHeight));
+				todo.add(new Rectangle(r.x, r.y + halfHeight, halfWidth, r.height - halfHeight));
+				todo.add(new Rectangle(r.x + halfWidth, r.y + halfHeight, r.width - halfWidth, r.height - halfHeight));
+			}
+		}
+		debugPrint("Done:", "time:", timer.toc(), "memory:", usedMemory());
+		
+		DensityTest.normalize(result, result, +0.0F, +255.0F);
+		
+		ImageComponent.showAdjusted(imageId, image, result);
+	}
+	
+	public static final Statistics computeStatistics(final Image image, final int firstRowIndex, final int firstColumnIndex, final int endRowIndex, final int endColumnIndex) {
+		final Statistics result = new Statistics();
+		
+		for (int rowIndex = firstRowIndex; rowIndex < endRowIndex; ++rowIndex) {
+			for (int columnIndex = firstColumnIndex; columnIndex < endColumnIndex; ++columnIndex) {
+				result.addValue(image.getValue(rowIndex, columnIndex));
+			}
+		}
+		
+		return result;
 	}
 	
 }
