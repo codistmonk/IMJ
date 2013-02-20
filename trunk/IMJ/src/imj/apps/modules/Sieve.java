eba00ef7ca1da14bfc2cf8956f6bb3a0491f48b2
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-02-20 16:31:12 +0000

    [IMJ] Added preview feature to Show.

--- trunk/IMJ/src/imj/apps/modules/Sieve.java
+++ trunk/IMJ/src/imj/apps/modules/Sieve.java
@@ -1,24 +1,45 @@
 package imj.apps.modules;
 
+import static net.sourceforge.aprog.tools.Tools.debugPrint;
+import static net.sourceforge.aprog.tools.Tools.usedMemory;
 import imj.Image;
+
+import java.util.Date;
+
 import net.sourceforge.aprog.context.Context;
+import net.sourceforge.aprog.tools.TicToc;
 
 /**
  * @author codistmonk (creation 2013-02-18)
  */
 public abstract class Sieve extends Plugin {
 	
+	private RegionOfInterest backupROI;
+	
+	private Sieve backupSieve;
+	
 	protected Sieve(final Context context) {
 		super(context);
 	}
 	
-	@Override
-	public final void apply() {
+	public final RegionOfInterest getROI() {
 		final Context context = this.getContext();
-		
 		final RegionOfInterest[] rois = context.get("rois");
 		final int lod = context.get("lod");
-		final RegionOfInterest roi = lod < rois.length ? rois[lod] : null;
+		
+		return lod < rois.length ? rois[lod] : null;
+	}
+	
+	@Override
+	public final void apply() {
+		final TicToc timer = new TicToc();
+		
+		debugPrint("Applying", this, "(", new Date(timer.tic()), ")");
+		
+		this.cancel();
+		
+		final Context context = this.getContext();
+		final RegionOfInterest roi = this.getROI();
 		
 		if (roi != null) {
 			final int rowCount = roi.getRowCount();
@@ -38,6 +59,29 @@ public abstract class Sieve extends Plugin {
 				context.set("sieve", this);
 			}
 		}
+		
+		debugPrint("Done:", "time:", timer.toc(), "memory:", usedMemory());
+	}
+	
+	@Override
+	public final void backup() {
+		final RegionOfInterest roi = this.getROI();
+		this.backupROI = new RegionOfInterest(roi.getRowCount(), roi.getColumnCount());
+		this.backupSieve = this.getContext().get("sieve");
+		
+		roi.copyTo(this.backupROI);
+	}
+	
+	@Override
+	public final void cancel() {
+		this.backupROI.copyTo(this.getROI());
+		this.getContext().set("sieve", this.backupSieve);
+	}
+	
+	@Override
+	public final void clearBackup() {
+		this.backupROI = null;
+		this.backupSieve = null;
 	}
 	
 	public abstract boolean accept(int index, int value);
