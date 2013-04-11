eb0518bb5c9e5946fce2668b86851de73cf8e72e
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-04-11 18:38:29 +0000

    [IMJ] Added HistogramDatabaseClusterViewFilter.

--- trunk/IMJ/src/imj/clustering/Distance.java
+++ trunk/IMJ/src/imj/clustering/Distance.java
@@ -11,6 +11,8 @@ public abstract interface Distance {
 	
 	public abstract double getDistance(double[] sample1, double[] sample2);
 	
+	public abstract double getDistance(int[] sample1, int[] sample2);
+	
 	/**
 	 * @author codistmonk (creation 2013-03-31)
 	 */
@@ -20,6 +22,18 @@ public abstract interface Distance {
 			
 			@Override
 			public final double getDistance(final double[] sample1, final double[] sample2) {
+				final int n = sample1.length;
+				double sumOfSquares = 0.0;
+				
+				for (int i = 0; i < n; ++i) {
+					sumOfSquares += square(sample2[i] - sample1[i]);
+				}
+				
+				return sqrt(sumOfSquares);
+			}
+			
+			@Override
+			public final double getDistance(final int[] sample1, final int[] sample2) {
 				final int n = sample1.length;
 				double sumOfSquares = 0.0;
 				
@@ -44,6 +58,18 @@ public abstract interface Distance {
 				return sumOfSquares / 2.0;
 			}
 			
+			@Override
+			public final double getDistance(final int[] sample1, final int[] sample2) {
+				final int n = sample1.length;
+				double sumOfSquares = 0.0;
+				
+				for (int i = 0; i < n; ++i) {
+					sumOfSquares += (1.0 + square(sample2[i] - sample1[i])) / (1.0 + abs(sample1[i] + sample2[i]));
+				}
+				
+				return sumOfSquares / 2.0;
+			}
+			
 		}, CITYBLOCK {
 			
 			@Override
@@ -58,10 +84,38 @@ public abstract interface Distance {
 				return result;
 			}
 			
+			@Override
+			public final double getDistance(final int[] sample1, final int[] sample2) {
+				final int n = sample1.length;
+				double result = 0.0;
+				
+				for (int i = 0; i < n; ++i) {
+					result += abs(sample2[i] - sample1[i]);
+				}
+				
+				return result;
+			}
+			
 		}, CHESSBOARD {
 			
 			@Override
 			public final double getDistance(final double[] sample1, final double[] sample2) {
+				final int n = sample1.length;
+				double result = 0.0;
+				
+				for (int i = 0; i < n; ++i) {
+					final double d = abs(sample2[i] - sample1[i]);
+					
+					if (result < d) {
+						result = d;
+					}
+				}
+				
+				return result;
+			}
+			
+			@Override
+			public final double getDistance(final int[] sample1, final int[] sample2) {
 				final int n = sample1.length;
 				double result = 0.0;
 				
