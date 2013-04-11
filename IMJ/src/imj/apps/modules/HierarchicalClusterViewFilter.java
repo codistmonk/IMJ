76c858849f141fd2cf5adce91814670927fd7d3c
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-04-11 10:30:37 +0000

    [IMJ] Updated HierarchicalClusterViewFilter (added parameter to specify processing scope).

--- IMJ/src/imj/apps/modules/HierarchicalClusterViewFilter.java
+++ IMJ/src/imj/apps/modules/HierarchicalClusterViewFilter.java
@@ -15,10 +15,13 @@ import imj.clustering.HierarchicalClusterer;
 import imj.clustering.HierarchicalClusterer.InterNode;
 import imj.clustering.HierarchicalClusterer.Node;
 
+import java.awt.Rectangle;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.PriorityQueue;
+
+import javax.swing.JComponent;
 
 import net.sourceforge.aprog.context.Context;
 import net.sourceforge.aprog.tools.TicToc;
@@ -30,6 +33,10 @@ import net.sourceforge.aprog.tools.Tools;
 public final class HierarchicalClusterViewFilter extends ViewFilter {
 	
 	private Channel[] channels;
+	
+	private String scope;
+	
+	private Image source;
 	
 	private int verticalTileCount;
 	
@@ -56,6 +63,7 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 		
 		this.getParameters().put("channels", "brightness");
 		this.getParameters().put("tiling", "16");
+		this.getParameters().put("scope", "viewport");
 		this.getParameters().put("sampling", "tile");
 		this.getParameters().put("clustering", "count 2");
 	}
@@ -103,17 +111,11 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 		}
 		
 		final int oldVerticalTileCount = this.verticalTileCount;
-		final int oldHorizontalTileCount = this.horizontalTileCount;
 		final String[] tiling = this.getParameters().get("tiling").split("\\s+");
 		this.verticalTileCount = parseInt(tiling[0]);
+		this.horizontalTileCount = this.verticalTileCount;
 		
-		if (this.getImage().getSource() != null) {
-			final int imageRowCount = this.getImage().getRowCount();
-			final int imageColumnCount = this.getImage().getColumnCount();
-			this.horizontalTileCount = imageColumnCount / (imageRowCount / this.verticalTileCount);
-		} else {
-			this.horizontalTileCount = this.verticalTileCount;
-		}
+		this.scope = this.getParameters().get("scope").trim().toUpperCase(ENGLISH);
 		
 		final String oldSampling = this.sampling;
 		final int oldChannelBinningBitCount = this.channelBinningBitCount;
@@ -133,25 +135,37 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 			this.diameter = parseDouble(clustering[1]);
 		}
 		
+		this.updateSource();
+		
 		return !Arrays.equals(oldChannels, this.channels) ||
-				oldVerticalTileCount != this.verticalTileCount || oldHorizontalTileCount != this.horizontalTileCount ||
+				oldVerticalTileCount != this.verticalTileCount ||
 				!Tools.equals(oldSampling, this.sampling) || oldChannelBinningBitCount != this.channelBinningBitCount;
 	}
 	
-	private final void updateClusters() {
-		final Image source = this.getImage().getSource();
+	private final void updateSource() {
+		this.source = this.getImage().getSource();
 		
-		if (source != null) {
+		if (this.source != null && "VIEWPORT".equals(this.scope)) {
+			final JComponent imageView = this.getContext().get("imageView");
+			this.source = new Viewport(this.source, imageView.getVisibleRect());
+			final int sourceRowCount = this.source.getRowCount();
+			final int sourceColumnCount = this.source.getColumnCount();
+			this.horizontalTileCount = sourceColumnCount / (sourceRowCount / this.verticalTileCount);
+		}
+	}
+	
+	private final void updateClusters() {
+		if (this.source != null) {
 			final TicToc timer = new TicToc();
 			
 			debugPrint("Collecting data...", "(" + new Date(timer.tic()) + ")");
 			
 			this.clusterer = new HierarchicalClusterer(Distance.Predefined.EUCLIDEAN);
 			final PixelProcessor collector;
-			final int imageRowCount = source.getRowCount();
-			final int imageColumnCount = source.getColumnCount();
-			final int tileRowCount = imageRowCount / this.verticalTileCount;
-			final int tileColumnCount = imageColumnCount / this.horizontalTileCount;
+			final int sourceRowCount = this.source.getRowCount();
+			final int sourceColumnCount = this.source.getColumnCount();
+			final int tileRowCount = sourceRowCount / this.verticalTileCount;
+			final int tileColumnCount = sourceColumnCount / this.horizontalTileCount;
 			final int tileSize = tileRowCount * tileColumnCount;
 			final int channelCount = this.channels.length;
 			final long[] processed = { 0L };
@@ -165,7 +179,7 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 					@Override
 					public final void process(final int pixel) {
 						++processed[0];
-						this.i = HierarchicalClusterViewFilter.this.collectChannelValues(source.getValue(pixel), this.i, sample);
+						this.i = HierarchicalClusterViewFilter.this.collectChannelValues(HierarchicalClusterViewFilter.this.source.getValue(pixel), this.i, sample);
 						
 						if (this.i == tileSize * channelCount) {
 							HierarchicalClusterViewFilter.this.clusterer.addSample(sample.clone());
@@ -184,7 +198,7 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 					@Override
 					public final void process(final int pixel) {
 						++processed[0];
-						HierarchicalClusterViewFilter.this.updateHistogram(source.getValue(pixel), sample);
+						HierarchicalClusterViewFilter.this.updateHistogram(HierarchicalClusterViewFilter.this.source.getValue(pixel), sample);
 						
 						if (++this.i == tileSize) {
 							HierarchicalClusterViewFilter.this.clusterer.addSample(sample.clone());
@@ -198,9 +212,9 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 				throw new IllegalArgumentException();
 			}
 			
-			forEachPixelInEachTile(source, this.verticalTileCount, this.horizontalTileCount, collector);
+			forEachPixelInEachTile(this.source, this.verticalTileCount, this.horizontalTileCount, collector);
 			
-			debugPrint(processed[0], "/", source.getRowCount() * source.getColumnCount());
+			debugPrint(processed[0], "/", this.source.getRowCount() * this.source.getColumnCount());
 			
 			debugPrint("Collecting data done", "(time:", timer.toc(), "memory:", usedMemory() + ")");
 			
@@ -255,12 +269,30 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 	}
 	
 	private final int getClusterIndex(final int pixel) {
-		final int imageRowCount = this.getImage().getRowCount();
-		final int imageColumnCount = this.getImage().getColumnCount();
-		final int pixelRowIndex = pixel / imageColumnCount;
-		final int pixelColumnIndex = pixel % imageColumnCount;
-		final int tileRowCount = imageRowCount / this.verticalTileCount;
-		final int tileColumnCount = imageColumnCount / this.horizontalTileCount;
+		final int sourceRowCount = this.source.getRowCount();
+		final int sourceColumnCount = this.source.getColumnCount();
+		final int pixelRowIndex;
+		final int pixelColumnIndex;
+		final Viewport viewport = cast(Viewport.class, this.source);
+		
+		if (viewport != null) {
+			final int imageColumnCount = this.getImage().getColumnCount();
+			final int pixelRowIndexInImage = pixel / imageColumnCount;
+			final int pixelColumnIndexInImage = pixel % imageColumnCount;
+			
+			if (!viewport.getViewport().contains(pixelColumnIndexInImage, pixelRowIndexInImage)) {
+				return 0;
+			}
+			
+			pixelRowIndex = viewport.convertRowIndex(pixelRowIndexInImage);
+			pixelColumnIndex = viewport.convertColumnIndex(pixelColumnIndexInImage);
+		} else {
+			pixelRowIndex = pixel / sourceColumnCount;
+			pixelColumnIndex = pixel % sourceColumnCount;
+		}
+		
+		final int tileRowCount = sourceRowCount / this.verticalTileCount;
+		final int tileColumnCount = sourceColumnCount / this.horizontalTileCount;
 		final int tileRowIndex = min(this.verticalTileCount - 1, pixelRowIndex / tileRowCount);
 		final int tileColumnIndex = min(this.horizontalTileCount - 1, pixelColumnIndex / tileColumnCount);
 		
@@ -326,6 +358,109 @@ public final class HierarchicalClusterViewFilter extends ViewFilter {
 		debugPrint("Extracting data done", "(time:", timer.toc(), "memory:", usedMemory() + ")");
 		
 		debugPrint("clusterCount:", this.clusterCount);
+	}
+	
+	/**
+	 * @author codistmonk (creation 2013-04-11)
+	 */
+	public static final class Viewport implements Image {
+		
+		private final Image source;
+		
+		private final Rectangle viewport;
+		
+		public Viewport(final Image source, final Rectangle viewport) {
+			this.source = source;
+			this.viewport = viewport;
+		}
+		
+		public final Image getSource() {
+			return this.source;
+		}
+		
+		public final Rectangle getViewport() {
+			return this.viewport;
+		}
+		
+		public final int convertRowIndex(final int rowIndexInSource) {
+			return rowIndexInSource - this.getViewport().y;
+		}
+		
+		public final int convertColumnIndex(final int columnIndexInSource) {
+			return columnIndexInSource - this.getViewport().x;
+		}
+		
+		public final int convertIndex(final int indexInSource) {
+			final int sourceColumnCount = this.getSource().getColumnCount();
+			final int rowIndexInSource = indexInSource / sourceColumnCount;
+			final int columnIndexInSource = indexInSource % sourceColumnCount;
+			
+			return this.convertRowIndex(rowIndexInSource) * this.getColumnCount() + this.convertColumnIndex(columnIndexInSource);
+		}
+		
+		@Override
+		public final int getChannelCount() {
+			return this.getSource().getChannelCount();
+		}
+		
+		@Override
+		public final int getRowCount() {
+			return this.getViewport().height;
+		}
+		
+		@Override
+		public final int getColumnCount() {
+			return this.getViewport().width;
+		}
+		
+		@Override
+		public final int getValue(final int rowIndex, final int columnIndex) {
+			return this.getSource().getValue(this.getViewport().y + rowIndex, this.getViewport().x + columnIndex);
+		}
+		
+		@Override
+		public final int setValue(final int rowIndex, final int columnIndex, final int value) {
+			return this.getSource().setValue(this.getViewport().y + rowIndex, this.getViewport().x + columnIndex, value);
+		}
+		
+		@Override
+		public final int getValue(final int index) {
+			final int columnCount = this.getColumnCount();
+			final int rowIndex = index / columnCount;
+			final int columnIndex = index % columnCount;
+			
+			return this.getValue(rowIndex, columnIndex);
+		}
+		
+		@Override
+		public final int setValue(final int index, final int value) {
+			final int columnCount = this.getColumnCount();
+			final int rowIndex = index / columnCount;
+			final int columnIndex = index % columnCount;
+			
+			return this.setValue(rowIndex, columnIndex, value);
+		}
+		
+		@Override
+		public final float getFloatValue(final int rowIndex, final int columnIndex) {
+			return Float.intBitsToFloat(this.getValue(rowIndex, columnIndex));
+		}
+		
+		@Override
+		public final float setFloatValue(final int rowIndex, final int columnIndex, final float value) {
+			return this.setValue(rowIndex, columnIndex, Float.floatToIntBits(value));
+		}
+		
+		@Override
+		public final float getFloatValue(final int index) {
+			return Float.intBitsToFloat(this.getValue(index));
+		}
+		
+		@Override
+		public final float setFloatValue(int index, float value) {
+			return this.setValue(index, Float.floatToIntBits(value));
+		}
+		
 	}
 	
 }
