6bce72635668e1033414577c3fa34842b075ddb4
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-04-22 08:19:33 +0000

    [IMJ] Updated TileDatabaseTest.

--- trunk/IMJ/test/imj/apps/modules/TileDatabaseTest.java
+++ trunk/IMJ/test/imj/apps/modules/TileDatabaseTest.java
@@ -4,17 +4,21 @@ import static imj.IMJTools.forEachPixelInEachTile;
 import static imj.apps.modules.ViewFilter.Channel.Primitive.BLUE;
 import static imj.apps.modules.ViewFilter.Channel.Primitive.GREEN;
 import static imj.apps.modules.ViewFilter.Channel.Primitive.RED;
+import static java.util.Arrays.sort;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
 import static net.sourceforge.aprog.tools.Tools.unchecked;
 import imj.IMJTools.PixelProcessor;
 import imj.Image;
 import imj.ImageWrangler;
+import imj.apps.modules.TileDatabaseTest.Sampler.SampleProcessor;
 import imj.apps.modules.ViewFilter.Channel;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -41,28 +45,19 @@ public class TileDatabaseTest {
 		
 		debugPrint(verticalTileCount, horizontalTileCount);
 		
-		forEachPixelInEachTile(image, verticalTileCount, horizontalTileCount, new PixelProcessor() {
-			
-			private final int[] sample = new int[tilePixelCount * channels.length];
-			
-			private int i;
+		final SampleProcessor processor = new SampleProcessor() {
 			
 			@Override
-			public final void process(final int pixel) {
-				final int pixelValue = image.getValue(pixel);
-				
-				for (final Channel channel : channels) {
-					this.sample[this.i++] = channel.getValue(pixelValue);
-				}
-				
-				if (this.sample.length <= this.i) {
-					this.i = 0;
-					
-					histogram.count(this.sample);
-				}
+			public final void process(final int[] sample) {
+				histogram.count(sample);
 			}
 			
-		});
+		};
+		
+//		forEachPixelInEachTile(image, verticalTileCount, horizontalTileCount,
+//				new LinearSampler(image, channels, tilePixelCount, processor));
+		forEachPixelInEachTile(image, verticalTileCount, horizontalTileCount,
+				new CompactHistogramSampler(image, channels, tilePixelCount, processor));
 		
 		debugPrint(histogram.getSampleCount());
 		debugPrint(countBytes(histogram));
@@ -88,27 +83,221 @@ public class TileDatabaseTest {
 			throw unchecked(exception);
 		}
 		
-		
 		return result[0];
 	}
 	
+	/**
+	 * @author codistmonk (creation 2013-04-22)
+	 */
+	public static abstract class Sampler implements PixelProcessor {
+		
+		private final Image image;
+		
+		private final Channel[] channels;
+		
+		private final int[] sample;
+		
+		private final SampleProcessor processor;
+		
+		protected Sampler(final Image image, final Channel[] channels, final int sampleSize, final SampleProcessor processor) {
+			this.image = image;
+			this.channels = channels;
+			this.sample = new int[sampleSize];
+			this.processor = processor;
+		}
+		
+		public final Image getImage() {
+			return this.image;
+		}
+		
+		public final Channel[] getChannels() {
+			return this.channels;
+		}
+		
+		public final int[] getSample() {
+			return this.sample;
+		}
+		
+		public final SampleProcessor getProcessor() {
+			return this.processor;
+		}
+		
+		/**
+		 * @author codistmonk (creation 2013-04-22)
+		 */
+		public static abstract interface SampleProcessor {
+			
+			public abstract void process(int[] sample);
+			
+		}
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2013-04-22)
+	 */
+	public static final class LinearSampler extends Sampler {
+		
+		private final int tilePixelCount;
+		
+		private int i;
+		
+		public LinearSampler(final Image image, final Channel[] channels,
+				final int tilePixelCount, final SampleProcessor processor) {
+			super(image, channels, tilePixelCount * channels.length, processor);
+			this.tilePixelCount = tilePixelCount;
+		}
+		
+		@Override
+		public final void process(final int pixel) {
+			final int pixelValue = this.getImage().getValue(pixel);
+			
+			for (final Channel channel : this.getChannels()) {
+				this.getSample()[this.i++] = channel.getValue(pixelValue);
+			}
+			
+			if (this.tilePixelCount <= this.i) {
+				this.i = 0;
+				
+				this.getProcessor().process(this.getSample());
+			}
+		}
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2013-04-22)
+	 */
+	public static final class CompactHistogramSampler extends Sampler {
+		
+		private final int tilePixelCount;
+		
+		private final Map<Integer, int[]> histogram;
+		
+		private final Integer[] indices;
+		
+		private final int[] values;
+		
+		private final int[] counts;
+		
+		private int i;
+		
+		public CompactHistogramSampler(final Image image, final Channel[] channels,
+				final int tilePixelCount, final SampleProcessor processor) {
+			super(image, channels, tilePixelCount * (channels.length + 1), processor);
+			this.tilePixelCount = tilePixelCount;
+			this.histogram = new HashMap<Integer, int[]>();
+			this.indices = new Integer[tilePixelCount];
+			this.values = new int[tilePixelCount];
+			this.counts = new int[tilePixelCount];
+		}
+		
+		private final void count(final int value) {
+			int[] count = this.histogram.get(value);
+			
+			if (count == null) {
+				count = new int[] { 1 };
+				this.histogram.put(value, count);
+			} else {
+				++count[0];
+			}
+		}
+		
+		@Override
+		public final void process(final int pixel) {
+			this.count(computeIndex(this.getImage().getValue(pixel), this.getChannels()));
+			
+			if (this.tilePixelCount <= ++this.i) {
+				this.i = 0;
+//				final int m = max(this.histogram);
+				
+				for (int j = 0; j < this.indices.length; ++j) {
+					this.indices[j] = j;
+					this.values[j] = 0;
+					this.counts[j] = 0;
+				}
+				
+				{
+					int j = 0;
+					
+					for (final Map.Entry<Integer, int[]> entry : this.histogram.entrySet()) {
+						this.values[j] = entry.getKey();
+						this.counts[j] = entry.getValue()[0];
+						++j;
+					}
+					
+					this.histogram.clear();
+				}
+				
+				final int[] h = this.counts;
+				
+				sort(this.indices, new Comparator<Integer>() {
+					
+					@Override
+					public final int compare(final Integer i1, final Integer i2) {
+						return h[i2] - h[i1];
+					}
+					
+				});
+				
+				for (int j = 0, k = 0; j < this.getSample().length; ++k) {
+					final int index = this.indices[k];
+					int value = this.values[index];
+					final int count = this.counts[index];
+					
+					for (int channelIndex = this.getChannels().length - 1; 0 <= channelIndex; --channelIndex) {
+						this.getSample()[j++] = value & 0x000000FF;
+						value >>= 8;
+					}
+					
+					this.getSample()[j++] = count;
+				}
+				
+				this.getProcessor().process(this.getSample());
+			}
+		}
+		
+		public static final int max(final int[] values) {
+			int result = Integer.MIN_VALUE;
+			
+			for (final int value : values) {
+				if (result < value) {
+					result = value;
+				}
+			}
+			
+			return result;
+		}
+		
+		public static final int computeIndex(final int pixelValue, final Channel... channels) {
+			int result = 0;
+			
+			for (final Channel channel : channels) {
+				result = (result << 8) | channel.getValue(pixelValue);
+			}
+			
+			return result;
+		}
+		
+	}
+
 	/**
 	 * @author codistmonk (creation 2013-04-19)
 	 */
 	public static final class SparseHistogram implements Serializable {
 		
-		private final Map<Integer, Object> data;
+		private final Map<Integer, Object> root;
 		
 		private int sampleCount;
 		
 		public SparseHistogram() {
-			this.data = newMap();
+			this.root = newMap();
 		}
 		
 		public final void count(final int... sample) {
 			final int n = sample.length;
 			final int lastIndex = n - 1;
-			Map<Integer, Object> node = this.data;
+			Map<Integer, Object> node = this.root;
 			
 			for (int i = 0; i < lastIndex; ++i) {
 				Map<Integer, Object> next = (Map<Integer, Object>) node.get(sample[i]);
