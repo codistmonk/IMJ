155e56f9ec59b637d2ae31151bd5272553382cd0
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-04-25 05:01:41 +0000

    [IMJ] Updated tests.

--- IMJ/test/imj/apps/modules/CompactIntMap.java
+++ IMJ/test/imj/apps/modules/CompactIntMap.java
@@ -1,7 +1,9 @@
 package imj.apps.modules;
 
 import static imj.IMJTools.deepToString;
+import static java.lang.Math.abs;
 import static java.util.Arrays.copyOf;
+import static java.util.Arrays.fill;
 import static java.util.Collections.binarySearch;
 import static net.sourceforge.aprog.tools.Tools.array;
 
@@ -10,6 +12,7 @@ import imj.IMJTools;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -25,12 +28,14 @@ public final class CompactIntMap {
 	
 	public CompactIntMap() {
 		this.chunks = new ArrayList<Chunk>(1);
+//		this.chunks = new LinkedList<Chunk>();
 	}
 	
 	public final Object get(final int index) {
 		int i = binarySearch(this.chunks, new Chunk(index, null));
 		
 		if (0 <= i) {
+//			return Special.convert(this.chunks.get(i).getValues()[0]);
 			return this.chunks.get(i).getValues()[0];
 		}
 		
@@ -47,6 +52,7 @@ public final class CompactIntMap {
 			return null;
 		}
 		
+//		return Special.convert(chunk.getValues()[j]);
 		return chunk.getValues()[j];
 	}
 	
@@ -72,47 +78,75 @@ public final class CompactIntMap {
 			final int n = left.getValues().length;
 			
 			if (j < n) {
+//				if (left.getValues()[j] == Special.NULL) {
+				if (left.getValues()[j] == null) {
+					++this.elementCount;
+				}
+				
 				left.getValues()[j] = value;
 				
 				return;
 			}
 			
-			mergeLeft = j == n;
+//			mergeLeft = j == n;
+			mergeLeft = j < n + MINIMUM_CHUNK_SIZE;
 		} else {
 			left = null;
 		}
 		
 		if (i < this.getChunkCount()) {
 			right = this.chunks.get(i);
-			mergeRight = index + 1 == right.getStart();
+//			mergeRight = index + 1 == right.getStart();
+			mergeRight = right.getStart() - MINIMUM_CHUNK_SIZE <= index;
 		} else {
 			right = null;
 		}
 		
+		mergeLeft = mergeRight = false;
+		
 		if (mergeLeft && mergeRight) {
 			final int n1 = left.getValues().length;
 			final int n2 = right.getValues().length;
-			final Chunk newChunk = new Chunk(left.getStart(), copyOf(left.getValues(), n1 + 1 + n2));
-			System.arraycopy(right.getValues(), 0, newChunk.getValues(), n1 + 1, n2);
-			newChunk.getValues()[n1] = value;
+//			final Chunk newChunk = new Chunk(left.getStart(), copyOf(left.getValues(), n1 + 1 + n2));
+//			System.arraycopy(right.getValues(), 0, newChunk.getValues(), n1 + 1, n2);
+//			newChunk.getValues()[n1] = value;
+			final Chunk newChunk = new Chunk(left.getStart(), copyOf(left.getValues(), n1 + MINIMUM_CHUNK_SIZE + n2));
+//			fill(newChunk.getValues(), n1, n1 + MINIMUM_CHUNK_SIZE, Special.NULL);
+			System.arraycopy(right.getValues(), 0, newChunk.getValues(), n1 + MINIMUM_CHUNK_SIZE, n2);
+			newChunk.getValues()[index - newChunk.getStart()] = value;
 			this.chunks.set(i - 1, newChunk);
 			this.chunks.remove(i);
 		} else if (mergeLeft) {
 			final int n = left.getValues().length;
-			final Chunk newChunk = new Chunk(left.getStart(), copyOf(left.getValues(), n + 1));
-			newChunk.getValues()[n] = value;
+//			final Chunk newChunk = new Chunk(left.getStart(), copyOf(left.getValues(), n + 1));
+//			newChunk.getValues()[n] = value;
+			final Chunk newChunk = new Chunk(left.getStart(), copyOf(left.getValues(), n + MINIMUM_CHUNK_SIZE));
+//			fill(newChunk.getValues(), n, n + MINIMUM_CHUNK_SIZE, Special.NULL);
+			newChunk.getValues()[index - newChunk.getStart()] = value;
 			this.chunks.set(i - 1, newChunk);
 		} else if (mergeRight) {
 			final int n = right.getValues().length;
-			final Chunk newChunk = new Chunk(index, new Object[n + 1]);
-			System.arraycopy(right.getValues(), 0, newChunk.getValues(), 1, n);
-			newChunk.getValues()[0] = value;
+//			final Chunk newChunk = new Chunk(index, new Object[n + 1]);
+//			System.arraycopy(right.getValues(), 0, newChunk.getValues(), 1, n);
+//			newChunk.getValues()[0] = value;
+			final Chunk newChunk = new Chunk(index & MINIMUM_CHUNK_SIZE_MASK, new Object[n + MINIMUM_CHUNK_SIZE]);
+//			fill(newChunk.getValues(), 0, right.getStart() - newChunk.getStart(), Special.NULL);
+			System.arraycopy(right.getValues(), 0, newChunk.getValues(), right.getStart() - newChunk.getStart(), n);
+			newChunk.getValues()[index - newChunk.getStart()] = value;
 			this.chunks.set(i, newChunk);
 		} else {
-			this.chunks.add(i, new Chunk(index, array(value)));
+//			this.chunks.add(i, new Chunk(index, array(value)));
+			final Chunk newChunk = new Chunk(index & MINIMUM_CHUNK_SIZE_MASK, new Object[MINIMUM_CHUNK_SIZE]);
+//			fill(newChunk.getValues(), Special.NULL);
+			newChunk.getValues()[index - newChunk.getStart()] = value;
+			this.chunks.add(i, newChunk);
 		}
 		
 		++this.elementCount;
+	}
+	
+	public static final int signum(final int x) {
+		return x < 0 ? -1 : x == 0 ? 0 : +1;
 	}
 	
 	public final int getElementCount() {
@@ -145,13 +179,15 @@ public final class CompactIntMap {
 					public final Entry next() {
 						if (this.chunk == null || this.chunk.getValues().length <= this.i) {
 							this.chunk = this.chunkIterator.next();
-							this.i = 0;
+//							this.i = 0;
+							this.i = nextValidIndex(this.chunk.getValues(), 0);
 						}
 						
 						entry.setKey(this.chunk.getStart() + this.i);
 						entry.setValue(this.chunk.getValues()[this.i]);
 						
-						++this.i;
+//						++this.i;
+						this.i = nextValidIndex(this.chunk.getValues(), this.i + 1);
 						
 						return entry;
 					}
@@ -167,9 +203,43 @@ public final class CompactIntMap {
 		};
 	}
 	
+	public static final int nextValidIndex(final Object[] array, final int start) {
+		int i = start;
+		
+//		while (i < array.length && array[i] == Special.NULL) {
+		while (i < array.length && array[i] == null) {
+			++i;
+		}
+		
+		return i;
+	}
+	
 	final int getChunkCount() {
 		return this.chunks.size();
 	}
+	
+	/**
+	 * {@value}.
+	 */
+	static final int MINIMUM_CHUNK_SIZE = 1 << 3;
+	
+	/**
+	 * {@value}.
+	 */
+	static final int MINIMUM_CHUNK_SIZE_MASK = ~(MINIMUM_CHUNK_SIZE - 1);
+	
+//	/**
+//	 * @author codistmonk (creation 2013-04-24)
+//	 */
+//	static enum Special {
+//		
+//		NULL;
+//		
+//		public static final Object convert(final Object object) {
+//			return object == NULL ? null : object;
+//		}
+//		
+//	}
 	
 	/**
 	 * @author codistmonk (creation 2013-04-24)
