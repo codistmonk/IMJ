85792007579b9f4c93b2fd24aee9c8e70d595f6a
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-04-25 05:01:41 +0000

    [IMJ] Updated tests.

--- trunk/IMJ/test/imj/apps/modules/CompactIntMapTest.java
+++ trunk/IMJ/test/imj/apps/modules/CompactIntMapTest.java
@@ -16,80 +16,98 @@ public class CompactIntMapTest {
 	public final void test1() {
 		final CompactIntMap map = new CompactIntMap();
 		
-		assertEquals(0, map.getElementCount());
-		assertEquals(0, map.getChunkCount());
 		assertEquals(null, map.get(0));
 		assertEquals(null, map.get(1));
 		assertEquals(null, map.get(2));
 		assertEquals(null, map.get(3));
 		assertEquals(null, map.get(4));
+		assertEquals(0, map.getElementCount());
+		assertEquals(0, map.getChunkCount());
 		assertEquals("[]", toStrings(map.entries()).toString());
 		
 		map.put(0, "B");
 		
-		assertEquals(1, map.getElementCount());
-		assertEquals(1, map.getChunkCount());
 		assertEquals("B", map.get(0));
 		assertEquals(null, map.get(1));
 		assertEquals(null, map.get(2));
 		assertEquals(null, map.get(3));
 		assertEquals(null, map.get(4));
+		assertEquals(1, map.getElementCount());
 		assertEquals("[0=B]", toStrings(map.entries()).toString());
+		
+		if (CompactIntMap.MINIMUM_CHUNK_SIZE == 1) {
+			assertEquals(1, map.getChunkCount());
+		}
 		
 		map.put(0, "A");
 		
-		assertEquals(1, map.getElementCount());
-		assertEquals(1, map.getChunkCount());
 		assertEquals("A", map.get(0));
 		assertEquals(null, map.get(1));
 		assertEquals(null, map.get(2));
 		assertEquals(null, map.get(3));
 		assertEquals(null, map.get(4));
+		assertEquals(1, map.getElementCount());
 		assertEquals("[0=A]", toStrings(map.entries()).toString());
+		
+		if (CompactIntMap.MINIMUM_CHUNK_SIZE == 1) {
+			assertEquals(1, map.getChunkCount());
+		}
 		
 		map.put(3, "D");
 		
-		assertEquals(2, map.getElementCount());
-		assertEquals(2, map.getChunkCount());
 		assertEquals("A", map.get(0));
 		assertEquals(null, map.get(1));
 		assertEquals(null, map.get(2));
 		assertEquals("D", map.get(3));
 		assertEquals(null, map.get(4));
+		assertEquals(2, map.getElementCount());
 		assertEquals("[0=A, 3=D]", toStrings(map.entries()).toString());
+		
+		if (CompactIntMap.MINIMUM_CHUNK_SIZE == 1) {
+			assertEquals(2, map.getChunkCount());
+		}
 		
 		map.put(4, "E");
 		
-		assertEquals(3, map.getElementCount());
-		assertEquals(2, map.getChunkCount());
 		assertEquals("A", map.get(0));
 		assertEquals(null, map.get(1));
 		assertEquals(null, map.get(2));
 		assertEquals("D", map.get(3));
 		assertEquals("E", map.get(4));
+		assertEquals(3, map.getElementCount());
 		assertEquals("[0=A, 3=D, 4=E]", toStrings(map.entries()).toString());
+		
+		if (CompactIntMap.MINIMUM_CHUNK_SIZE == 1) {
+			assertEquals(2, map.getChunkCount());
+		}
 		
 		map.put(2, "C");
 		
-		assertEquals(4, map.getElementCount());
-		assertEquals(2, map.getChunkCount());
 		assertEquals("A", map.get(0));
 		assertEquals(null, map.get(1));
 		assertEquals("C", map.get(2));
 		assertEquals("D", map.get(3));
 		assertEquals("E", map.get(4));
+		assertEquals(4, map.getElementCount());
 		assertEquals("[0=A, 2=C, 3=D, 4=E]", toStrings(map.entries()).toString());
+		
+		if (CompactIntMap.MINIMUM_CHUNK_SIZE == 1) {
+			assertEquals(2, map.getChunkCount());
+		}
 		
 		map.put(1, "B");
 		
-		assertEquals(5, map.getElementCount());
-		assertEquals(1, map.getChunkCount());
 		assertEquals("A", map.get(0));
 		assertEquals("B", map.get(1));
 		assertEquals("C", map.get(2));
 		assertEquals("D", map.get(3));
 		assertEquals("E", map.get(4));
+		assertEquals(5, map.getElementCount());
 		assertEquals("[0=A, 1=B, 2=C, 3=D, 4=E]", toStrings(map.entries()).toString());
+		
+		if (CompactIntMap.MINIMUM_CHUNK_SIZE == 1) {
+			assertEquals(1, map.getChunkCount());
+		}
 	}
 	
 	public static final <E> List<String> toStrings(final Iterable<E> elements) {
