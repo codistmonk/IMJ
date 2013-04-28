de35851109ce40fb6616fa1c9a41c38ff66e543e
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-04-28 11:01:03 +0000

    [IMJ] Updated tests.

--- trunk/IMJ/test/imj/apps/modules/BKSearch.java
+++ trunk/IMJ/test/imj/apps/modules/BKSearch.java
@@ -4,10 +4,10 @@ import static java.lang.Math.abs;
 import static java.lang.Math.min;
 import static java.util.Arrays.binarySearch;
 import static java.util.Arrays.sort;
-import static net.sourceforge.aprog.tools.Tools.debugPrint;
+
+import java.util.Comparator;
 
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
-import net.sourceforge.aprog.tools.Tools;
 
 /**
  * @author codistmonk (creation 2013-04-28)
@@ -20,12 +20,12 @@ public final class BKSearch {
 	
 	public static final long[] bkSort(final int[] values) {
 		final int n = values.length;
-		final ValueDistance[] vds = new ValueDistance[n];
+		final IntDistant[] vds = new IntDistant[n];
 		final int v0 = values[0];
 		
 		for (int i = 0; i < n; ++i) {
 			final int vi = values[i];
-			vds[i] = new ValueDistance(vi, distance(v0, vi));
+			vds[i] = new IntDistant(vi, distance(v0, vi));
 		}
 		
 		sort(vds);
@@ -33,7 +33,7 @@ public final class BKSearch {
 		final long[] result = new long[n];
 		
 		for (int i = 0; i < n; ++i) {
-			final ValueDistance vd = vds[i];
+			final IntDistant vd = vds[i];
 			values[i] = vd.getValue();
 			result[i] = vd.getDistance();
 		}
@@ -84,12 +84,95 @@ public final class BKSearch {
 		return bkValues[i0];
 	}
 	
+	public static final <T> long[] bkSort(final T[] values, final Distance<T> distance, final Comparator<T> comparator) {
+		final int n = values.length;
+		@SuppressWarnings("unchecked")
+		final GenericDistant<T>[] vds = new GenericDistant[n];
+		final T v0 = values[0];
+		
+		for (int i = 0; i < n; ++i) {
+			final T vi = values[i];
+			vds[i] = new GenericDistant<T>(vi, distance.getDistance(v0, vi), comparator);
+		}
+		
+		sort(vds);
+		
+		final long[] result = new long[n];
+		
+		for (int i = 0; i < n; ++i) {
+			final GenericDistant<T> vd = vds[i];
+			values[i] = vd.getValue();
+			result[i] = vd.getDistance();
+		}
+		
+		return result;
+	}
+	
+	public static final <T> T bkFind(final T value, final T[] bkValues, final long[] bkDistances, final Distance<T> distance) {
+		final int n = bkValues.length;
+		final long vv0 = distance.getDistance(value, bkValues[0]);
+		int i0 = binarySearch(bkDistances, vv0);
+		
+		if (i0 < 0) {
+			i0 = min(n - 1, - (i0 + 1));
+		}
+		
+		final int i1 = i0;
+		long vvi0 = distance.getDistance(value, bkValues[i0]);
+		
+		for (int j = i0 - 1; 0 <= j; --j) {
+			long vjv0 = bkDistances[j];
+			long vvj = distance.getDistance(value, bkValues[j]);
+			
+			if (vvj < vvi0) {
+				i0 = j;
+				vvi0 = vvj;
+			}
+			
+			if (vjv0 < bkDistances[i0] - 2 * vvi0) {
+				break;
+			}
+		}
+		
+		for (int j = i1 + 1; j < n; ++j) {
+			long vjv0 = bkDistances[j];
+			long vvj = distance.getDistance(value, bkValues[j]);
+			
+			if (vvj < vvi0) {
+				i0 = j;
+				vvi0 = vvj;
+			}
+			
+			if (bkDistances[i0] + 2 * vvi0 < vjv0) {
+				break;
+			}
+		}
+		
+		return bkValues[i0];
+	}
+	
 	public static final int findClosest(final int value, final int[] values) {
 		long closestDistance = Long.MAX_VALUE;
 		int result = 0;
 		
 		for (final int v : values) {
-			final long d = abs((long) value - v);
+			final long d = distance(value, v);
+			
+			if (d < closestDistance) {
+				closestDistance = d;
+				result = v;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static final <T> T findClosest(final T value, final T[] values, final Distance<T> distance) {
+		long closestDistance = Long.MAX_VALUE;
+		T result = null;
+		
+		for (final T v : values) {
+			final long d = distance.getDistance(value, v);
 			
 			if (d < closestDistance) {
 				closestDistance = d;
@@ -111,13 +194,13 @@ public final class BKSearch {
 	/**
 	 * @author codistmonk (creation 2013-04-27)
 	 */
-	private static final class ValueDistance implements Comparable<ValueDistance> {
+	private static final class IntDistant implements Comparable<IntDistant> {
 		
 		private final int value;
 		
 		private final long distance;
 		
-		public ValueDistance(final int value, final long distance) {
+		public IntDistant(final int value, final long distance) {
 			this.value = value;
 			this.distance = distance;
 		}
@@ -131,7 +214,7 @@ public final class BKSearch {
 		}
 		
 		@Override
-		public final int compareTo(final ValueDistance that) {
+		public final int compareTo(final IntDistant that) {
 			int result = signum(this.getDistance() - that.getDistance());
 			
 			if (result == 0) {
@@ -140,6 +223,55 @@ public final class BKSearch {
 			
 			return result;
 		}
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2013-04-27)
+	 */
+	private static final class GenericDistant<T> implements Comparable<GenericDistant<T>> {
+		
+		private final T value;
+		
+		private final long distance;
+		
+		private final Comparator<T> comparator;
+		
+		public GenericDistant(final T value, final long distance, final Comparator<T> comparator) {
+			this.value = value;
+			this.distance = distance;
+			this.comparator = comparator;
+		}
+		
+		public final T getValue() {
+			return this.value;
+		}
+		
+		public final long getDistance() {
+			return this.distance;
+		}
+		
+		@Override
+		public final int compareTo(final GenericDistant<T> that) {
+			int result = signum(this.getDistance() - that.getDistance());
+			
+			if (result == 0) {
+				return this.comparator.compare(this.getValue(), that.getValue());
+			}
+			
+			return result;
+		}
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2013-04-28)
+	 *
+	 * @param <T>
+	 */
+	public static abstract interface Distance<T> {
+		
+		public abstract long getDistance(T object0, T object1);
 		
 	}
 	
--- trunk/IMJ/test/imj/apps/modules/BKSearchTest.java
+++ trunk/IMJ/test/imj/apps/modules/BKSearchTest.java
@@ -4,9 +4,16 @@ import static imj.apps.modules.BKSearch.bkFind;
 import static imj.apps.modules.BKSearch.bkSort;
 import static imj.apps.modules.BKSearch.distance;
 import static imj.apps.modules.BKSearch.findClosest;
+import static java.lang.Math.ceil;
+import static java.lang.Math.sqrt;
 import static java.util.Arrays.sort;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import imj.apps.modules.BKSearch.Distance;
+
+import java.util.Arrays;
+import java.util.Random;
 
 import net.sourceforge.aprog.tools.TicToc;
 
@@ -29,7 +36,7 @@ public final class BKSearchTest {
 			
 			sort(sortedValues);
 			
-			final long[] distances = bkSort(bkValues);
+			final long[] bkDistances = bkSort(bkValues);
 			
 			debugPrint("n:", n);
 			
@@ -41,19 +48,118 @@ public final class BKSearchTest {
 			
 			timer.tic();
 			for (final int value : values) {
-				assertEquals(value, bkFind(value, bkValues, distances));
+				assertEquals(value, bkFind(value, bkValues, bkDistances));
 			}
 			debugPrint("time:", timer.toc());
 			
 			for (final int value : moreValues) {
 				final int linearClosest = findClosest(value, sortedValues);
-				final int bkClosest = bkFind(value, bkValues, distances);
+				final int bkClosest = bkFind(value, bkValues, bkDistances);
 				final long expected = distance(value, linearClosest);
 				final long actual = distance(value, bkClosest);
 				
 				assertEquals(expected, actual);
 			}
 		}
+	}
+	
+	@Test
+	public final void test2() {
+		final TicToc timer = new TicToc();
+		final EuclideanDistance distance = EuclideanDistance.INSTANCE;
+		
+		for (int n = 10; n <= 20000; n *= 2) {
+			final byte[][] values = newRandomSamples(2, n);
+			final byte[][] moreValues = newRandomSamples(2, n);
+//			final byte[][] values = {{65, 71}, {-59, -84}, {-31, 69}, {47, 52}, {-3, 119}, {104, -91}};
+//			final byte[][] moreValues = {{107, -34}, {27, -35}, {-101, -74}, {-120, -36}, {-97, -42}, {-88, -85}};
+			
+			final byte[][] sortedValues = clone(values);
+			final byte[][] bkValues = clone(values);
+			
+			sort(sortedValues, ByteArrayComparator.INSTANCE);
+			
+			final long[] bkDistances = bkSort(bkValues, distance, ByteArrayComparator.INSTANCE);
+			
+			debugPrint("n:", n);
+			
+			timer.tic();
+			for (final byte[] value : values) {
+				assertArrayEquals(value, findClosest(value, sortedValues, distance));
+			}
+			debugPrint("time:", timer.toc());
+			
+			timer.tic();
+			for (final byte[] value : values) {
+				assertArrayEquals(value, bkFind(value, bkValues, bkDistances, distance));
+			}
+			debugPrint("time:", timer.toc());
+			
+			for (final byte[] value : moreValues) {
+				final byte[] linearClosest = findClosest(value, sortedValues, distance);
+				final byte[] bkClosest = bkFind(value, bkValues, bkDistances, distance);
+				final long expected = distance.getDistance(value, linearClosest);
+				final long actual = distance.getDistance(value, bkClosest);
+				
+				if (expected != actual) {
+					debugPrint(Arrays.deepToString(values));
+					debugPrint(Arrays.deepToString(moreValues));
+					debugPrint(Arrays.deepToString(bkValues));
+					debugPrint(Arrays.toString(bkDistances));
+					debugPrint(Arrays.toString(value), distance.getDistance(value, bkValues[0]));
+					debugPrint(Arrays.toString(linearClosest), expected);
+					debugPrint(Arrays.toString(bkClosest), actual);
+				}
+				
+				assertEquals(expected, actual);
+			}
+		}
+	}
+	
+	public static final byte[][] clone(final byte[][] array) {
+		final byte[][] result = new byte[array.length][array[0].length];
+		
+		for (int i = 0; i < result.length; ++i) {
+			result[i] = array[i].clone();
+		}
+		
+		return result;
+	}
+	
+	public static final byte[][] newRandomSamples(final int dimension, final int sampleCount) {
+		final Random random = new Random();
+		final byte[][] result = new byte[sampleCount][dimension];
+		
+		for (final byte[] sample : result) {
+			random.nextBytes(sample);
+		}
+		
+		return result;
+	}
+	
+	public static final long square(final int x) {
+		return (long) x * x;
+	}
+	
+	/**
+	 * @author codistmonk (creation 2013-04-28)
+	 */
+	public static final class EuclideanDistance implements Distance<byte[]> {
+		
+		@Override
+		public final long getDistance(final byte[] sample0, final byte[] sample1) {
+			long result = 0L;
+			final int n = sample0.length;
+			
+			for (int i = 0; i < n; ++i) {
+				result += square(sample1[i] - sample0[i]);
+			}
+			
+			return (long) ceil(sqrt(result));
+		}
+		
+		public static final EuclideanDistance INSTANCE = new EuclideanDistance();
+		
 	}
 	
 }
