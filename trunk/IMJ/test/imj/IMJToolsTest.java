0cc1118932919ef5fba5604e5839c735f620a27b
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-05-15 14:43:41 +0000

    [IMJ] Updated new "forEachPixelInEachComponent" algorithm.

--- trunk/IMJ/test/imj/IMJToolsTest.java
+++ trunk/IMJ/test/imj/IMJToolsTest.java
@@ -3,10 +3,12 @@ package imj;
 import static imj.IMJTools.forEachPixelInEachComponent4b;
 import static java.lang.Long.bitCount;
 import static java.lang.Long.toBinaryString;
+import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import imj.IMJTools.PixelProcessor;
 import imj.apps.modules.RegionOfInterest;
+import net.sourceforge.aprog.tools.Tools;
 
 import org.junit.Test;
 
@@ -22,7 +24,7 @@ public class IMJToolsTest {
 				0, 0, 0,
 				1, 0, 1);
 		
-		if (true) {
+		{
 			final PixelCounter counter = new PixelCounter();
 			
 			forEachPixelInEachComponent4b(roi, false, counter);
@@ -46,7 +48,7 @@ public class IMJToolsTest {
 				1, 1, 0,
 				1, 0, 1);
 		
-		if (false) {
+		{
 			final PixelCounter counter = new PixelCounter();
 			
 			forEachPixelInEachComponent4b(roi, false, counter);
@@ -65,6 +67,55 @@ public class IMJToolsTest {
 	
 	@Test
 	public final void testComponentIteration3() {
+		final Image roi = roi(3,
+				1, 0, 0,
+				1, 0, 1,
+				1, 1, 1);
+		
+		{
+			final PixelCounter counter = new PixelCounter();
+			
+			forEachPixelInEachComponent4b(roi, false, counter);
+			
+			assertArrayEquals(new int[] { 6 }, counter.getCounts().toArray());
+		}
+		
+		{
+			final PixelCounter counter = new PixelCounter();
+			
+			forEachPixelInEachComponent4b(roi, true, counter);
+			
+			assertArrayEquals(new int[] { 9 }, counter.getCounts().toArray());
+		}
+	}
+	
+	@Test
+	public final void testComponentIteration4() {
+		final Image roi = roi(4,
+				1, 0, 0, 1,
+				0, 1, 0, 1,
+				0, 0, 1, 1,
+				0, 0, 0, 0);
+		
+		{
+			final PixelCounter counter = new PixelCounter();
+			
+			forEachPixelInEachComponent4b(roi, false, counter);
+			
+			assertArrayEquals(new int[] { 1, 4, 1 }, counter.getCounts().toArray());
+		}
+		
+		{
+			final PixelCounter counter = new PixelCounter();
+			
+			forEachPixelInEachComponent4b(roi, true, counter);
+			Tools.debugPrint(counter.getCounts());
+			assertArrayEquals(new int[] { 6, 6, 4 }, counter.getCounts().toArray());
+		}
+	}
+	
+	@Test
+	public final void testComponentIteration5() {
 		for (int n = 1; n <= 4; ++n) {
 			testAllBinarySquares(n);
 		}
@@ -72,13 +123,8 @@ public class IMJToolsTest {
 	
 	public static final void testAllBinarySquares(final int n) {
 		for (long i = 0L; i < ~((~0L) << (n * n)); ++i) {
-			final RegionOfInterest roi = new RegionOfInterest.UsingBitSet(n, n, false);
-			
-			for (int j = 0; j < n * n; ++j) {
-				if (0L != ((i >> (n * n - 1L - j)) & 1L)) {
-					roi.set(j);
-				}
-			}
+			final RegionOfInterest roi = newROI(n, i);
+			final int componentCount;
 			
 			{
 				final PixelCounter counter = new PixelCounter();
@@ -86,6 +132,8 @@ public class IMJToolsTest {
 				forEachPixelInEachComponent4b(roi, false, counter);
 				
 				assertEquals("n: " + n + ", i: " + toBinaryString(i), bitCount(i), counter.getPixelCount());
+				
+				componentCount = counter.getCounts().size();
 			}
 			
 			{
@@ -94,8 +142,26 @@ public class IMJToolsTest {
 				forEachPixelInEachComponent4b(roi, true, counter);
 				
 				assertEquals("n: " + n + ", i:" + toBinaryString(i), roi.getPixelCount(), counter.getPixelCount());
+				
+				if (0L != (i >> (n * n - 1L))) {
+					assertEquals("n: " + n + ", i:" + toBinaryString(i), componentCount, counter.getCounts().size());
+				} else {
+					assertTrue("n: " + n + ", i:" + toBinaryString(i), componentCount <= counter.getCounts().size());
+				}
 			}
 		}
+	}
+	
+	public static final RegionOfInterest newROI(final int n, long bits) {
+		final RegionOfInterest result = new RegionOfInterest.UsingBitSet(n, n, false);
+		
+		for (int i = 0; i < n * n; ++i) {
+			if (0L != ((bits >> (n * n - 1L - i)) & 1L)) {
+				result.set(i);
+			}
+		}
+		
+		return result;
 	}
 	
 	public static final RegionOfInterest.UsingBitSet roi(final int columnCount, final int... values) {
