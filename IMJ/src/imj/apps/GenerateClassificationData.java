06536e10505225a57f2ad2e8677903b1efc4bbfc
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-06-05 08:11:02 +0000

    [IMJ] Updated  GenerateClassificationData.

--- IMJ/src/imj/apps/GenerateClassificationData.java
+++ IMJ/src/imj/apps/GenerateClassificationData.java
@@ -1,5 +1,6 @@
 package imj.apps;
 
+import static imj.IMJTools.loadAndTryToCache;
 import static imj.apps.modules.ShowActions.baseName;
 import static imj.database.IMJDatabaseTools.RGB;
 import static imj.database.IMJDatabaseTools.checkDatabase;
@@ -10,9 +11,8 @@ import static imj.database.IMJDatabaseTools.newRGBSampler;
 import static imj.database.IMJDatabaseTools.updateNegativeGroups;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
 import static net.sourceforge.aprog.tools.Tools.gc;
-
+import static net.sourceforge.aprog.tools.Tools.unchecked;
 import imj.Image;
-import imj.ImageWrangler;
 import imj.IntList;
 import imj.apps.GenerateSampleDatabase.Configuration;
 import imj.apps.modules.Annotations;
@@ -27,12 +27,16 @@ import imj.database.Sampler;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
+import net.sourceforge.aprog.tools.MathTools.Statistics;
 import net.sourceforge.aprog.tools.TicToc;
 
 /**
@@ -47,49 +51,106 @@ public final class GenerateClassificationData {
 	/**
 	 * @param commandLineArguments
 	 * <br>Must not be null
-	 * @throws Exception 
 	 */
-	public static final void main(final String[] commandLineArguments) throws Exception {
+	public static final void main(final String[] commandLineArguments) {
 		final Configuration configuration = new Configuration(commandLineArguments);
 		final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser(commandLineArguments);
 		final TicToc timer = new TicToc();
-		final String[] trainingSet = arguments.get("using", "").split(",");
-		final String testImageId = arguments.get("on", "");
-		final PatchDatabase<Sample> sampleDatabase = new PatchDatabase<Sample>(Sample.class);
+		final String[] trainingSet0 = arguments.get("using", "").split(",");
+		final String testImageId0 = arguments.get("on", "");
+		final String[][] trainingSets;
+		final String[] testImageIds;
 		
-		System.out.println("Loading training set... " + new Date(timer.tic()));
-		
-		for (final String trainingImageId : trainingSet) {
-			final ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
-					trainingImageId + configuration.getSuffix()));
+		if ("".equals(testImageId0)) {
+			final int n = trainingSet0.length;
+			trainingSets = new String[n][];
+			testImageIds = new String[n];
 			
-			try {
-				@SuppressWarnings("unchecked")
-				final Map<Object, Object> database = (Map<Object, Object>) ois.readObject();
-				@SuppressWarnings("unchecked")
-				final PatchDatabase<Sample> samples = (PatchDatabase<Sample>) database.get("samples");
-				
-				for (final Map.Entry<byte[], Sample> entry : samples) {
-					final Sample sample = entry.getValue();
-					final Sample newSample = sampleDatabase.add(entry.getKey());
-					
-					newSample.getClasses().addAll(sample.getClasses());
-					newSample.incrementCount(sample.getCount());
-				}
-			} finally {
-				ois.close();
+			for (int i = 0; i < n; ++i) {
+				trainingSets[i] = remove(i, trainingSet0);
+				testImageIds[i] = trainingSet0[i];
 			}
+		} else {
+			trainingSets = new String[][] { trainingSet0 };
+			testImageIds = new String[] { testImageId0 };
 		}
 		
-		updateNegativeGroups(sampleDatabase);
+		final Map<String, ConfusionTable[]> allConfusionTables = new HashMap<String, ConfusionTable[]>();
 		
-		System.out.println("Loading training set done, time: " + timer.toc());
+		for (int i = 0; i < trainingSets.length; ++i) {
+			final String[] trainingSet = trainingSets[i];
+			final String testImageId = testImageIds[i];
+			final PatchDatabase<Sample> sampleDatabase = new PatchDatabase<Sample>(Sample.class);
+			
+			System.out.println("trainingSet: " + Arrays.toString(trainingSet));
+			System.out.println("testImageId: " + testImageId);
+			
+			System.out.println("Loading training set... " + new Date(timer.tic()));
+			
+			loadTrainingSet(configuration, trainingSet, sampleDatabase);
+			
+			System.out.println("Loading training set done, time: " + timer.toc());
+			
+			checkDatabase(sampleDatabase);
+			
+			System.out.println("Processing image... " + new Date(timer.tic()));
+			
+			final Map<String, ConfusionTable> confusionTables = computeConfusionTables(
+					configuration, testImageId, sampleDatabase);
+			
+			for (final Map.Entry<String, ConfusionTable> entry : confusionTables.entrySet()) {
+				ConfusionTable[] tables = allConfusionTables.get(entry.getKey());
+				
+				if (tables == null) {
+					tables = new ConfusionTable[trainingSets.length];
+					allConfusionTables.put(entry.getKey(), tables);
+				}
+				
+				tables[i] = entry.getValue();
+			}
+			
+			System.out.println("Processing image done, time: " + timer.toc());
+			System.out.println("confusionTables: " + confusionTables);
+		}
 		
-		checkDatabase(sampleDatabase);
+		for (final Map.Entry<String, ConfusionTable[]> entry : allConfusionTables.entrySet()) {
+			final Statistics fpr = new Statistics();
+			final Statistics tpr = new Statistics();
+			
+			for (final ConfusionTable table : entry.getValue()) {
+				if (0L < table.getActualNegative() && 0L < table.getActualPositive()) {
+					fpr.addValue(table.getFalsePositiveRate());
+					tpr.addValue(table.getTruePositiveRate());
+				}
+			}
+			
+			System.out.println("class: " + entry.getKey() + ", FPR: " + format(fpr) + ", TPR: " + format(tpr));
+		}
 		
-		System.out.println("Processing image... " + new Date(timer.tic()));
 		
-		final Image image = ImageWrangler.INSTANCE.load(testImageId, configuration.getLod());
+	}
+	
+	public static final String format(final Statistics statistics) {
+		return statistics.getMean() + "(" + Math.sqrt(statistics.getVariance()) + ")";
+	}
+	
+	public static final <T> T[] remove(final int index, final T[] array) {
+		try {
+			final T[] result = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length - 1);
+			
+			System.arraycopy(array, 0, result, 0, index);
+			System.arraycopy(array, index + 1, result, index, array.length - index - 1);
+			
+			return result;
+		} catch (final Exception exception) {
+			throw unchecked(exception);
+		}
+	}
+	
+	public static final Map<String, ConfusionTable> computeConfusionTables(
+			final Configuration configuration, final String testImageId,
+			final PatchDatabase<Sample> sampleDatabase) {
+		final Image image = loadAndTryToCache(testImageId, configuration.getLod());
 		final Quantizer quantizer = new BinningQuantizer();
 		final Channel[] channels = RGB;
 		
@@ -106,12 +167,12 @@ public final class GenerateClassificationData {
 		final int imageColumnCount = image.getColumnCount();
 		final Annotations annotations = Annotations.fromXML(baseName(testImageId) + ".xml");
 		final Map<String, RegionOfInterest> classes = new HashMap<String, RegionOfInterest>();
-		final Map<String, ConfusionTable> confusionTables = new HashMap<String, ConfusionTable>();
+		final Map<String, ConfusionTable> result = new HashMap<String, ConfusionTable>();
 		
 		loadRegions(testImageId, configuration.getLod(), imageRowCount, imageColumnCount, annotations, classes);
 		
 		for (final String key : classes.keySet()) {
-			confusionTables.put(key, new ConfusionTable());
+			result.put(key, new ConfusionTable());
 		}
 		
 		configuration.getSegmenter().process(image, new Sampler(image, quantizer, channels, collector) {
@@ -134,7 +195,7 @@ public final class GenerateClassificationData {
 				for (final Map.Entry<String, RegionOfInterest> entry : classes.entrySet()) {
 					final String key = entry.getKey();
 					final RegionOfInterest groundTruth = classes.get(key);
-					final ConfusionTable confusionTable = confusionTables.get(key);
+					final ConfusionTable confusionTable = result.get(key);
 					
 					if (sample.getClasses().contains(key)) {
 						// sample is positive
@@ -174,14 +235,44 @@ public final class GenerateClassificationData {
 			
 		});
 		
-		System.out.println("Processing image done, time: " + timer.toc());
-		System.out.println("confusionTables: " + confusionTables);
+		return result;
+	}
+	
+	public static final void loadTrainingSet(final Configuration configuration,
+			final String[] trainingSet, final PatchDatabase<Sample> sampleDatabase) {
+		try {
+			for (final String trainingImageId : trainingSet) {
+				final ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
+						baseName(trainingImageId) + configuration.getSuffix()));
+				
+				try {
+					@SuppressWarnings("unchecked")
+					final Map<Object, Object> database = (Map<Object, Object>) ois.readObject();
+					@SuppressWarnings("unchecked")
+					final PatchDatabase<Sample> samples = (PatchDatabase<Sample>) database.get("samples");
+					
+					for (final Map.Entry<byte[], Sample> entry : samples) {
+						final Sample sample = entry.getValue();
+						final Sample newSample = sampleDatabase.add(entry.getKey());
+						
+						newSample.getClasses().addAll(sample.getClasses());
+						newSample.incrementCount(sample.getCount());
+					}
+				} finally {
+					ois.close();
+				}
+			}
+			
+			updateNegativeGroups(sampleDatabase);
+		} catch (final Exception exception) {
+			throw unchecked(exception);
+		}
 	}
 	
 	/**
 	 * @author codistmonk (creation 2013-06-04)
 	 */
-	public static final class ConfusionTable {
+	public static final class ConfusionTable implements Serializable {
 		
 		private long truePositive;
 		
@@ -190,6 +281,30 @@ public final class GenerateClassificationData {
 		private long trueNegative;
 		
 		private long falseNegative;
+		
+		public final long getActualPositive() {
+			return this.getTruePositive() + this.getFalseNegative();
+		}
+		
+		public final long getActualNegative() {
+			return this.getFalsePositive() + this.getTrueNegative();
+		}
+		
+		public final long getPositive() {
+			return this.getTruePositive() + this.getFalsePositive();
+		}
+		
+		public final long getNegative() {
+			return this.getTrueNegative() + this.getFalseNegative();
+		}
+		
+		public final double getTruePositiveRate() {
+			return (double) this.getTruePositive() / this.getActualPositive();
+		}
+		
+		public final double getFalsePositiveRate() {
+			return (double) this.getFalsePositive() / this.getActualNegative();
+		}
 		
 		public final long getTruePositive() {
 			return this.truePositive;
@@ -228,6 +343,11 @@ public final class GenerateClassificationData {
 			return "{TP: " + this.getTruePositive() + ", FP: " + this.getFalsePositive() +
 					", TN: " + this.getTrueNegative() + ", FN: " + this.getFalseNegative() + "}";
 		}
+		
+		/**
+		 * {@value}.
+		 */
+		private static final long serialVersionUID = -2815956297294457592L;
 		
 	}
 	
