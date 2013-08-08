5e7b58be885a2a8bc5ea2580f1782d1719634447
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-08-08 07:49:35 +0000

    [IMJ][imj2] Updated tests.

--- trunk/IMJ/test/imj2/tools/IMJToolsTest.java
+++ trunk/IMJ/test/imj2/tools/IMJToolsTest.java
@@ -3,6 +3,7 @@ package imj2.tools;
 import static imj2.tools.IMJTools.quantize;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
 import static org.junit.Assert.assertEquals;
+
 import imj2.core.Image2D;
 import imj2.core.Image2D.MonopatchProcess;
 
@@ -19,7 +20,7 @@ import org.junit.Test;
 public final class IMJToolsTest {
 	
 	@Test
-	public final void testAWT() throws Exception {
+	public final void testAwt() throws Exception {
 		final String base = "test/imj2/data/";
 		final String[] ids = { "rgb24.gif", "rgb24.jpg", "rgb24.png", "rgb32.png" };
 		
@@ -51,7 +52,7 @@ public final class IMJToolsTest {
 	}
 	
 	@Test
-	public final void testLOCI1() throws Exception {
+	public final void testLoci1() throws Exception {
 		final String base = "test/imj2/data/";
 		final String[] ids = { "rgb24.jpg", "rgb24.png", "rgb24.ppm", "rgb24.bmp", "rgb24.gif", "rgb32.png" };
 		
@@ -63,6 +64,43 @@ public final class IMJToolsTest {
 			assertRGB(color, 255, 0, 0, image.getPixelValue(0, 0));
 			assertRGB(color, 0, 255, 0, image.getPixelValue(1, 0));
 			assertRGB(color, 0, 0, 255, image.getPixelValue(0, 1));
+		}
+	}
+	
+	@Test
+	public final void testLoci2() throws Exception {
+		final String base = "test/imj2/data/";
+		final String[] ids = { "gray8.png", "gray8.pgm" };
+		
+		for (final String id : ids) {
+			final String imageId = base + id;
+			final Image2D image = new LociBackedImage(imageId);
+			final DefaultColorModel color = new DefaultColorModel(image.getChannels());
+			
+			assertGray(color, 0, image.getPixelValue(0, 0));
+			assertGray(color, 84, image.getPixelValue(1, 0));
+			assertGray(color, 171, image.getPixelValue(0, 1));
+			assertGray(color, 255, image.getPixelValue(1, 1));
+		}
+	}
+	
+	@Test
+	public final void testLoci3() throws Exception {
+		final String base = "test/imj2/data/";
+		final String[] ids = { "bw1.pbm" };
+		
+		for (final String id : ids) {
+			final String imageId = base + id;
+			final Image2D image = new LociBackedImage(imageId);
+			final DefaultColorModel color = new DefaultColorModel(image.getChannels());
+			
+			debugPrint(image.getWidth(), image.getHeight(), image.getChannels());
+			
+//			assertEquals(PredefinedChannels.C1_U1, image.getChannels());
+			assertBinary(color, 0, image.getPixelValue(0, 0));
+			assertBinary(color, 1, image.getPixelValue(1, 0));
+			assertBinary(color, 1, image.getPixelValue(0, 1));
+			assertBinary(color, 0, image.getPixelValue(1, 1));
 		}
 	}
 	
@@ -78,7 +116,7 @@ public final class IMJToolsTest {
 		IMJTools.show(new LociBackedImage(imageId));
 	}
 	
-	@Test
+//	@Test
 	public final void testShow2() throws Exception {
 		final String imageId = "../Libraries/images/svs/16088.svs";
 		final Image2D image = new LociBackedImage(imageId);
@@ -92,6 +130,16 @@ public final class IMJToolsTest {
 		assertEquals(quantize(expectedRed, 2), quantize(color.red(actualPixelValue), 2));
 		assertEquals(quantize(expectedGreen, 2), quantize(color.green(actualPixelValue), 2));
 		assertEquals(quantize(expectedBlue, 2), quantize(color.blue(actualPixelValue), 2));
+	}
+	
+	public static final void assertGray(final DefaultColorModel color,
+			final int expectedGray, final int actualPixelValue) {
+		assertEquals(quantize(expectedGray, 2), quantize(color.gray(actualPixelValue), 2));
+	}
+	
+	public static final void assertBinary(final DefaultColorModel color,
+			final int expectedBinary, final int actualPixelValue) {
+		assertEquals(expectedBinary, color.binary(actualPixelValue));
 	}
 	
 }
