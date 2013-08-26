96aa4bfb5c43f3fbc2c867d2e0862dd57ae43132
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-08-26 21:14:21 +0000

    [IMJ][imj2] Refactoring.

--- IMJ/src/imj2/tools/SubsampledImage2D.java
+++ IMJ/src/imj2/tools/SubsampledImage2D.java
@@ -1,41 +1,28 @@
 package imj2.tools;
 
 import static java.lang.Integer.parseInt;
-import static net.sourceforge.aprog.tools.Tools.unchecked;
 
 import imj2.core.ConcreteImage2D;
 import imj2.core.Image2D;
-import imj2.core.LinearIntImage;
 
-import java.util.Arrays;
-import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @author codistmonk (creation 2013-08-25)
  */
-public final class SubsampledImage2D extends TiledImage2D {
-	
-	private final Image2D source;
+public final class SubsampledImage2D extends FilteredTiledImage2D {
 	
 	private final int width;
 	
 	private final int height;
 	
-	private Image2D tile;
-	
 	public SubsampledImage2D(final Image2D source) {
-		super(subId(source.getId()));
-		this.source = source;
+		super(subId(source.getId()), source);
 		this.width = source.getWidth() / 2;
 		this.height = source.getHeight() / 2;
 
 		this.useOptimalTileDimensionsOf(source, 256, 256);
-	}
-	
-	public final Image2D getSource() {
-		return this.source;
 	}
 	
 	@Override
@@ -59,69 +46,36 @@ public final class SubsampledImage2D extends TiledImage2D {
 	}
 	
 	@Override
-	protected final int getPixelValueFromTile(final int x, final int y, final int xInTile, final int yInTile) {
-		return this.tile.getPixelValue(xInTile, yInTile);
-	}
-	
-	@Override
-	protected final boolean makeNewTile() {
-		return this.tile == null;
-	}
-	
-	@Override
-	protected final void updateTile() {
-		this.tile = IMJTools.cache(Arrays.asList(this.getId(), this.getTileX(), this.getTileY()), new Callable<Image2D>() {
+	protected final Image2D updateTile(final Image2D tile) {
+		final Image2D source = this.getSource();
+		final DefaultColorModel color = new DefaultColorModel(source.getChannels());
+		final int tileX = this.getTileX();
+		final int tileY = this.getTileY();
+		
+		tile.forEachPixelInBox(tileX, tileY, tile.getWidth(), tile.getHeight(), new MonopatchProcess() {
 			
 			@Override
-			public final Image2D call() throws Exception {
-				return SubsampledImage2D.this.updateTile(SubsampledImage2D.this.newTile());
+			public final void pixel(final int x, final int y) {
+				final int rgba00 = source.getPixelValue(x * 2 + 0, y * 2 + 0);
+				final int rgba10 = source.getPixelValue(x * 2 + 1, y * 2 + 0);
+				final int rgba01 = source.getPixelValue(x * 2 + 0, y * 2 + 1);
+				final int rgba11 = source.getPixelValue(x * 2 + 1, y * 2 + 1);
+				final int red = (color.red(rgba00) + color.red(rgba10) + color.red(rgba01) + color.red(rgba11)) / 4;
+				final int green = (color.green(rgba00) + color.green(rgba10) + color.green(rgba01) + color.green(rgba11)) / 4;
+				final int blue = (color.blue(rgba00) + color.blue(rgba10) + color.blue(rgba01) + color.blue(rgba11)) / 4;
+				final int alpha = (color.alpha(rgba00) + color.alpha(rgba10) + color.alpha(rgba01) + color.alpha(rgba11)) / 4;
+				
+				tile.setPixelValue(x - tileX, y - tileY, DefaultColorModel.argb(red, green, blue, alpha));
 			}
 			
+			/**
+			 * {@value}.
+			 */
+			private static final long serialVersionUID = 8092549882515884605L;
+			
 		});
-	}
-	
-	final Image2D updateTile(final Image2D tile) {
-		try {
-			final Image2D source = this.getSource();
-			final DefaultColorModel color = new DefaultColorModel(source.getChannels());
-			final int tileX = this.getTileX();
-			final int tileY = this.getTileY();
-			
-			tile.forEachPixelInBox(tileX, tileY, tile.getWidth(), tile.getHeight(), new MonopatchProcess() {
-				
-				@Override
-				public final void pixel(final int x, final int y) {
-					final int rgba00 = source.getPixelValue(x * 2 + 0, y * 2 + 0);
-					final int rgba10 = source.getPixelValue(x * 2 + 1, y * 2 + 0);
-					final int rgba01 = source.getPixelValue(x * 2 + 0, y * 2 + 1);
-					final int rgba11 = source.getPixelValue(x * 2 + 1, y * 2 + 1);
-					final int red = (color.red(rgba00) + color.red(rgba10) + color.red(rgba01) + color.red(rgba11)) / 4;
-					final int green = (color.green(rgba00) + color.green(rgba10) + color.green(rgba01) + color.green(rgba11)) / 4;
-					final int blue = (color.blue(rgba00) + color.blue(rgba10) + color.blue(rgba01) + color.blue(rgba11)) / 4;
-					final int alpha = (color.alpha(rgba00) + color.alpha(rgba10) + color.alpha(rgba01) + color.alpha(rgba11)) / 4;
-					
-					tile.setPixelValue(x - tileX, y - tileY, DefaultColorModel.argb(red, green, blue, alpha));
-				}
-				
-				/**
-				 * {@value}.
-				 */
-				private static final long serialVersionUID = 8092549882515884605L;
-				
-			});
-			
-			return tile;
-		} catch (final Exception exception) {
-			throw unchecked(exception);
-		}
-	}
-	
-	final Image2D newTile() {
-		final int tileWidth = this.getTileWidth();
-		final int tileHeight = this.getTileHeight();
 		
-		return new ConcreteImage2D(
-				new LinearIntImage("", (long) tileWidth * tileHeight, this.getChannels()), tileWidth, tileHeight);
+		return tile;
 	}
 	
 	/**
