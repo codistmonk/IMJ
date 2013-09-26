df2a5fcc2042c960e9dcb99b7569ae42a68b8388
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-09-26 11:50:10 +0000

    [IMJ][imj] Updated ARFFExporterTest.

--- trunk/IMJ/test/imj/database/ARFFExporterTest.java
+++ trunk/IMJ/test/imj/database/ARFFExporterTest.java
@@ -1,25 +1,36 @@
 package imj.database;
 
+import static imj.IMJTools.argb;
+import static imj.IMJTools.unsigned;
+import static imj.apps.GenerateClassificationData.resetExcludedRegions;
 import static imj.apps.modules.ShowActions.baseName;
+import static imj.database.HistogramSampler.COUNT_QUANTIZATION_MASK;
 import static imj.database.IMJDatabaseTools.RGB;
 import static imj.database.IMJDatabaseTools.loadRegions;
+import static java.util.Collections.unmodifiableMap;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
 import static net.sourceforge.aprog.tools.Tools.gc;
 import static net.sourceforge.aprog.tools.Tools.unchecked;
-
 import imj.ByteList;
+import imj.IMJTools;
 import imj.Image;
 import imj.ImageWrangler;
+import imj.apps.GenerateClassificationData;
 import imj.apps.modules.Annotations;
 import imj.apps.modules.RegionOfInterest;
 import imj.apps.modules.ViewFilter.Channel;
 import imj.database.Sampler.SampleProcessor;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 import org.junit.Test;
 
@@ -70,17 +81,42 @@ public final class ARFFExporterTest {
 			loadRegions(imageId, lod, imageRowCount, imageColumnCount, annotations, classes);
 			
 			final Sampler sampler;
+			// TODO(codistmonk) write to file
+			// TODO(codistmonk) write ARFF header
+			final PrintStream out = System.out;
 			
 			try {
  				sampler = samplerFactory.getConstructor(Image.class, Quantizer.class, Channel[].class, SampleProcessor.class)
 						.newInstance(image, quantizers[i], channels, new SampleProcessor() {
 							
-							private final Collection<String> group = new HashSet<String>();
+							private final Collection<String> group = new TreeSet<String>();
 							
 							@Override
 							public final void processSample(final ByteList sample) {
-								// TODO(codistmonk) normalize group
-								// TODO(codistmonk) write ARFF line
+								final int n = sample.size();
+								
+								out.print("{");
+								
+								for (int i = 0; i < n; i += 4) {
+									final double frequency = unsigned(sample.get(i + 3)) / (double) COUNT_QUANTIZATION_MASK;
+									
+									if (0.0 != frequency) {
+										final int color = argb(0, unsigned(sample.get(i + 2)),
+												unsigned(sample.get(i + 1)), unsigned(sample.get(i + 0)));
+										
+										out.print(color);
+										out.print(" ");
+										out.print(frequency);
+										out.print(", ");
+									}
+								}
+								
+								out.print(1 << 24);
+								out.print(" ");
+								out.print(formatGroup(this.group));
+								
+								out.println("}");
+								
 								this.group.clear();
 							}
 							
@@ -88,7 +124,7 @@ public final class ARFFExporterTest {
 							public final void processPixel(final int pixel, final int pixelValue) {
 								for (final Map.Entry<String, RegionOfInterest> entry : classes.entrySet()) {
 									if (entry.getValue().get(pixel)) {
-										this.group.add(entry.getKey());
+										this.group.add(CLASS_ALIASES.get(entry.getKey()));
 									}
 								}
 							}
@@ -97,9 +133,50 @@ public final class ARFFExporterTest {
 			} catch (final Exception exception) {
 				throw unchecked(exception);
 			}
-
+			
 			trainingSegmenter.process(image, sampler);
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	static final Map<String, String> CLASS_ALIASES = unmodifiableMap((Map<String, String>) map(
+			"Edges & Artifacts to be excluded", "EXCLUDED",
+			"invasive tumor (diffusely infiltrating pre-existing tissues)", "DIFFUSE",
+			"Invasive tumor (solid formations)", "SOLID",
+			"Intersecting stromal bands", "STROMA",
+			"Non-neoplastic glands and ducts", "NONNEOPLASTIC",
+			"DCIS and invasive inside ductal structures", "DCIS",
+			null, "UNKNOWN"
+	));
+	
+	public static final LinkedHashMap<?, ?> map(final Object... keyAndValues) {
+		final int n = keyAndValues.length;
+		final LinkedHashMap<Object, Object> result = new LinkedHashMap<Object, Object>(n / 2);
+		
+		for (int i = 0; i < n; i += 2) {
+			result.put(keyAndValues[i], keyAndValues[i + 1]);
+		}
+		
+		return result;
+	}
+	
+	public static final String formatGroup(final Iterable<String> group) {
+		final StringBuilder resultBuilder = new StringBuilder();
+		final Iterator<String> i = group.iterator();
+		
+		resultBuilder.append('"');
+		
+		if (i.hasNext()) {
+			resultBuilder.append(i.next());
+			
+			while (i.hasNext()) {
+				resultBuilder.append(" ").append(i.next());
+			}
+		}
+		
+		resultBuilder.append('"');
+		
+		return resultBuilder.toString();
 	}
 	
 }
