8337ae300bc3f4cad9d7152080e2c870e2d6aa73
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2013-11-20 09:36:43 +0000

    [IMJ][imj2] Added Jsch and SFTPStreamHandlerFactory.

---
+++ trunk/IMJ/src/imj2/tools/SFTPStreamHandlerFactory.java
@@ -0,0 +1,192 @@
+package imj2.tools;
+
+import static java.util.Arrays.asList;
+import static java.util.Arrays.fill;
+import static java.util.Collections.synchronizedMap;
+import static net.sourceforge.aprog.swing.SwingTools.horizontalBox;
+import static net.sourceforge.aprog.swing.SwingTools.verticalBox;
+import static net.sourceforge.aprog.tools.Tools.debugPrint;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.prefs.Preferences;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import net.sourceforge.aprog.swing.SwingTools;
+import net.sourceforge.aprog.tools.Pair;
+import net.sourceforge.aprog.tools.Tools;
+
+/**
+ * @author codistmonk (creation 2013-11-20)
+ */
+public final class SFTPStreamHandlerFactory implements URLStreamHandlerFactory, Serializable {
+	
+	@Override
+	public final URLStreamHandler createURLStreamHandler(final String protocol) {
+		if ("sftp".equals(protocol)) {
+			return new SFTPStreamHandler();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * {@value}.
+	 */
+	private static final long serialVersionUID = 6645843997774044192L;
+	
+	/**
+	 * @author codistmonk (creation 2013-11-20)
+	 */
+	public static final class SFTPStreamHandler extends URLStreamHandler implements Serializable {
+		
+		@Override
+		protected final URLConnection openConnection(final URL url) throws IOException {
+			final Pair<String, byte[]> loginPassword = new Credentials(url.getHost()).getLoginPassword();
+			
+			if (loginPassword == null) {
+				return null;
+			}
+			
+			try {
+				final List<String> channelKey = asList(url.getHost(), loginPassword.getFirst());
+				final ChannelSftp channel;
+				final ChannelSftp cachedChannel = channels.get(channelKey);
+				
+				if (cachedChannel != null && cachedChannel.isConnected()) {
+					channel = cachedChannel;
+				} else {
+					final Session session = new JSch().getSession(loginPassword.getFirst(), url.getHost());
+					
+					session.setConfig("StrictHostKeyChecking", "no");
+					session.setPassword(loginPassword.getSecond());
+					session.connect();
+					
+					debugPrint("sessionConnected:", session.isConnected());
+					
+					channel = (ChannelSftp) session.openChannel("sftp");
+					
+					channel.connect();
+					
+					debugPrint("channelConnected:", channel.isConnected());
+					
+					if (channel.isConnected()) {
+						channels.put(channelKey, channel);
+					}
+				}
+				
+				return new URLConnection(url) {
+					
+					@Override
+					public final void connect() throws IOException {
+						// NOP
+					}
+					
+					@Override
+					public final InputStream getInputStream() throws IOException {
+						try {
+							return channel.get(this.getURL().getFile());
+						} catch (final SftpException exception) {
+							debugPrint(this.getURL());
+							throw new IOException(exception);
+						}
+					}
+					
+				};
+			} catch (final JSchException exception) {
+				exception.printStackTrace();
+			}
+			
+			return null;
+		}
+		
+		/**
+		 * {@value}.
+		 */
+		private static final long serialVersionUID = 6164151336427013720L;
+		
+		private static final Map<List<String>, ChannelSftp> channels = new HashMap<List<String>, ChannelSftp>();
+		
+		/**
+		 * @author codistmonk (creation 2013-11-18)
+		 */
+		public static final class Credentials implements Serializable {
+			
+			private transient Pair<String, byte[]> loginPassword;
+			
+			public Credentials(final String host) {
+				this.loginPassword = authorizations.get(host);
+				
+				if (this.loginPassword == null) {
+					SwingTools.setCheckAWT(false);
+					final Preferences preferences = Preferences.userNodeForPackage(Tools.getCallerClass());
+					final String userNameKey = "login for " + host;
+					final String userName = preferences.get(userNameKey, System.getProperty("user.name"));
+					final JTextField loginField = new JTextField(userName);
+					final JPasswordField passwordField = new JPasswordField();
+					final JComponent credentialsComponent = verticalBox(
+							horizontalBox(new JLabel("Login:"), loginField),
+							horizontalBox(new JLabel("Password:"), passwordField)
+					);
+					passwordField.requestFocusInWindow();
+					final int option = JOptionPane.showOptionDialog(null, credentialsComponent, host,
+							JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+					
+					SwingTools.setCheckAWT(true);
+					
+					if (option == JOptionPane.OK_OPTION) {
+						this.loginPassword = new Pair<String, byte[]>(loginField.getText(), getPassword(passwordField));
+						preferences.put(userNameKey, this.loginPassword.getFirst());
+						authorizations.put(host, this.loginPassword);
+					}
+				}
+			}
+			
+			public final Pair<String, byte[]> getLoginPassword() {
+				try {
+					return this.loginPassword;
+				} finally {
+					this.loginPassword = null;
+				}
+			}
+			
+			private static final Map<String, Pair<String, byte[]>> authorizations = synchronizedMap(new HashMap<String, Pair<String, byte[]>>());
+			
+			public static final byte[] getPassword(final JPasswordField passwordField) {
+				final char[] passwordChars = passwordField.getPassword();
+				final byte[] result = new String(passwordChars).getBytes();
+				
+				fill(passwordChars, (char) 0);
+				
+				return result;
+			}
+			
+			/**
+			 * {@value}.
+			 */
+			private static final long serialVersionUID = -435478800001672284L;
+			
+		}
+		
+	}
+	
+}
