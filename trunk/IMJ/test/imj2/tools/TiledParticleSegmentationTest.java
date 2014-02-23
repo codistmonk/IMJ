da2e82beffa587090f5d9af6cff69f3247a7b573
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2014-02-23 16:00:56 +0000

    [IMJ][imj2] Refactoring.

--- trunk/IMJ/test/imj2/tools/TiledParticleSegmentationTest.java
+++ trunk/IMJ/test/imj2/tools/TiledParticleSegmentationTest.java
@@ -2,8 +2,9 @@ package imj2.tools;
 
 import static imj2.tools.MultiresolutionSegmentationTest.getColorGradient;
 import static java.lang.Math.min;
+import static java.util.Arrays.fill;
 import static net.sourceforge.aprog.swing.SwingTools.show;
-
+import static net.sourceforge.aprog.tools.Tools.instances;
 import imj2.tools.Image2DComponent.Painter;
 import imj2.tools.RegionShrinkingTest.AutoMouseAdapter;
 import imj2.tools.RegionShrinkingTest.SimpleImageView;
@@ -12,6 +13,10 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
+
+import net.sourceforge.aprog.tools.Factory.DefaultFactory;
+import net.sourceforge.aprog.tools.Tools;
 
 import org.junit.Test;
 
@@ -19,6 +24,26 @@ import org.junit.Test;
  * @author codistmonk (creation 2014-02-23)
  */
 public final class TiledParticleSegmentationTest {
+	
+	/**
+	 * {@value}.
+	 */
+	public static final int NORTH = 0;
+	
+	/**
+	 * {@value}.
+	 */
+	public static final int WEST = 1;
+	
+	/**
+	 * {@value}.
+	 */
+	public static final int EAST = 2;
+	
+	/**
+	 * {@value}.
+	 */
+	public static final int SOUTH = 3;
 	
 	@Test
 	public final void test() {
@@ -41,56 +66,39 @@ public final class TiledParticleSegmentationTest {
 					final int imageWidth = image.getWidth();
 					final int imageHeight = image.getHeight();
 					final int s = getCellSize();
+					final Point[] particles = instances(4, DefaultFactory.forClass(Point.class));
+					final int[] maximumGradients = new int[particles.length];
 					
 					for (int tileY = 0; tileY < imageHeight; tileY += s) {
 						final int tileLastY = min(imageHeight - 1, tileY + s);
 						
 						for (int tileX = 0; tileX < imageWidth; tileX += s) {
 							final int tileLastX = min(imageWidth - 1, tileX + s);
-							final Point north = new Point((tileX + tileLastX) / 2, tileY);
-							final Point west = new Point(tileX, (tileY + tileLastY) / 2);
-							final Point east = new Point(tileLastX, west.y);
-							final Point south = new Point(north.x, tileLastY);
-							int northMaximumGradient = 0;
-							int westMaximumGradient = 0;
-							int eastMaximumGradient = 0;
-							int southMaximumGradient = 0;
+							
+							particles[NORTH].setLocation((tileX + tileLastX) / 2, tileY);
+							particles[WEST].setLocation(tileX, (tileY + tileLastY) / 2);
+							particles[EAST].setLocation(tileLastX, particles[WEST].y);
+							particles[SOUTH].setLocation(particles[NORTH].x, tileLastY);
+							
+							fill(maximumGradients, 0);
 							
 							for (int x = tileX + 1; x < tileLastX; ++x) {
-								final int northGradient = getColorGradient(image, x, tileY);
-								
-								if (northMaximumGradient < northGradient) {
-									northMaximumGradient = northGradient;
-									north.x = x;
-								}
-								
-								final int southGradient = getColorGradient(image, x, tileLastY);
-								
-								if (southMaximumGradient < southGradient) {
-									southMaximumGradient = southGradient;
-									south.x = x;
-								}
+								updateParticleX(NORTH, particles, maximumGradients, x,
+										getColorGradient(image, x, tileY));
+								updateParticleX(SOUTH, particles, maximumGradients, x,
+										getColorGradient(image, x, tileLastY));
 							}
 							
 							for (int y = tileY + 1; y < tileLastY; ++y) {
-								final int westGradient = getColorGradient(image, tileX, y);
-								
-								if (westMaximumGradient < westGradient) {
-									westMaximumGradient = westGradient;
-									west.y = y;
-								}
-								
-								final int eastGradient = getColorGradient(image, tileLastX, y);
-								
-								if (eastMaximumGradient < eastGradient) {
-									eastMaximumGradient = eastGradient;
-									east.y = y;
-								}
+								updateParticleY(WEST, particles, maximumGradients, y,
+										getColorGradient(image, tileX, y));
+								updateParticleY(EAST, particles, maximumGradients, y,
+										getColorGradient(image, tileLastX, y));
 							}
 							
 							g.setColor(Color.RED);
-							g.drawLine(north.x, north.y, south.x, south.y);
-							g.drawLine(west.x, west.y, east.x, east.y);
+							g.drawLine(particles[NORTH].x, particles[NORTH].y, particles[SOUTH].x, particles[SOUTH].y);
+							g.drawLine(particles[WEST].x, particles[WEST].y, particles[EAST].x, particles[EAST].y);
 						}
 					}
 				}
@@ -119,6 +127,22 @@ public final class TiledParticleSegmentationTest {
 		};
 		
 		show(imageView, this.getClass().getSimpleName(), true);
+	}
+	
+	public static final void updateParticleX(final int particleIndex, final Point[] particles,
+			final int[] maximumGradients, final int x, final int gradient) {
+		if (maximumGradients[particleIndex] < gradient) {
+			maximumGradients[particleIndex] = gradient;
+			particles[particleIndex].x = x;
+		}
+	}
+	
+	public static final void updateParticleY(final int particleIndex, final Point[] particles,
+			final int[] maximumGradients, final int y, final int gradient) {
+		if (maximumGradients[particleIndex] < gradient) {
+			maximumGradients[particleIndex] = gradient;
+			particles[particleIndex].y = y;
+		}
 	}
 	
 }
