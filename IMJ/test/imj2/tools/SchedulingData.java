c93e5caf694f0cb7b8b88ab9a7c79d565d2d0dbb
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2014-02-27 08:59:36 +0000

    [IMJ][imj2] Refactoring.

---
+++ IMJ/test/imj2/tools/SchedulingData.java
@@ -0,0 +1,50 @@
+package imj2.tools;
+
+import static java.lang.Thread.currentThread;
+import static net.sourceforge.aprog.tools.Tools.getOrCreate;
+
+import imj.IntList;
+
+import java.io.Serializable;
+import java.util.BitSet;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import net.sourceforge.aprog.tools.Factory.DefaultFactory;
+
+/**
+ * @author codistmonk (creation 2014-02-26)
+ */
+public final class SchedulingData implements Serializable {
+	
+	private final IntList todo;
+	
+	private final BitSet done;
+	
+	public SchedulingData() {
+		this.todo = new IntList();
+		this.done = new BitSet();
+	}
+	
+	public final IntList getTodo() {
+		return this.todo;
+	}
+	
+	public final BitSet getDone() {
+		return this.done;
+	}
+	
+	/**
+	 * {@value}.
+	 */
+	private static final long serialVersionUID = -3771171821850295230L;
+	
+	private static final Map<Thread, SchedulingData> reusableSchedulingData = new WeakHashMap<Thread, SchedulingData>();
+	
+	public static final DefaultFactory<SchedulingData> FACTORY = DefaultFactory.forClass(SchedulingData.class);
+	
+	public static final SchedulingData getInstance() {
+		return getOrCreate(reusableSchedulingData, currentThread(), FACTORY);
+	}
+	
+}
