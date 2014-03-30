c8f4944085d9827dce09ad6783f889b8ab74c170
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2014-03-30 14:31:40 +0000

    [IMJ][imj2] Refactoring.

--- IMJ/src/imj2/tools/BatchConvert.java
+++ IMJ/src/imj2/tools/BatchConvert.java
@@ -26,6 +26,58 @@ import net.sourceforge.aprog.tools.IllegalInstantiationException;
  */
 public final class BatchConvert {
 	
+	private BatchConvert() {
+		throw new IllegalInstantiationException();
+	}
+	
+	/**
+	 * @param commandLineArguments
+	 * <br>Unused
+	 */
+	public static final void main(final String[] commandLineArguments) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public final void run() {
+				final JFrame frame = new JFrame("IMJ Batch Convert");
+				final JTextArea textArea = new JTextArea();
+				
+				frame.setPreferredSize(new Dimension(256, 256));
+				frame.add(SwingTools.scrollable(textArea));
+				frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+				
+				textArea.setEditable(false);
+				textArea.setDropTarget(new DropTarget() {
+					
+					@Override
+					public final synchronized void drop(final DropTargetDropEvent event) {
+						for (final File file : SwingTools.getFiles(event)) {
+							MultiThreadTools.getExecutor().submit(new FileProcessor(file, textArea));
+						}
+					}
+					
+					/**
+					 * {@value}.
+					 */
+					private static final long serialVersionUID = -3511295868159683003L;
+					
+				});
+				
+				SwingTools.packAndCenter(frame).setVisible(true);
+				
+				frame.addWindowListener(new WindowAdapter() {
+					
+					@Override
+					public final void windowClosed(final WindowEvent event) {
+						MultiThreadTools.shutdownExecutor();
+					}
+					
+				});
+			}
+			
+		});
+	}
+	
 	/**
 	 * @author codistmonk (creation 2014-03-30)
 	 */
@@ -98,58 +150,6 @@ public final class BatchConvert {
 			});
 		}
 		
-	}
-
-	private BatchConvert() {
-		throw new IllegalInstantiationException();
-	}
-	
-	/**
-	 * @param commandLineArguments
-	 * <br>Unused
-	 */
-	public static final void main(final String[] commandLineArguments) {
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			@Override
-			public final void run() {
-				final JFrame frame = new JFrame("IMJ Batch Convert");
-				final JTextArea textArea = new JTextArea();
-				
-				frame.setPreferredSize(new Dimension(256, 256));
-				frame.add(SwingTools.scrollable(textArea));
-				frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-				
-				textArea.setEditable(false);
-				textArea.setDropTarget(new DropTarget() {
-					
-					@Override
-					public final synchronized void drop(final DropTargetDropEvent event) {
-						for (final File file : SwingTools.getFiles(event)) {
-							MultiThreadTools.getExecutor().submit(new FileProcessor(file, textArea));
-						}
-					}
-					
-					/**
-					 * {@value}.
-					 */
-					private static final long serialVersionUID = -3511295868159683003L;
-					
-				});
-				
-				SwingTools.packAndCenter(frame).setVisible(true);
-				
-				frame.addWindowListener(new WindowAdapter() {
-					
-					@Override
-					public final void windowClosed(final WindowEvent event) {
-						MultiThreadTools.shutdownExecutor();
-					}
-					
-				});
-			}
-			
-		});
 	}
 	
 }
