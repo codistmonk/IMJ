726a0548a10cb54f46110694f64eb6ea58205a02
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2014-04-28 16:38:59 +0000

    [IMJ] Refactoring.

--- IMJ/test/pixel3d/Pixel3DDemo.java
+++ IMJ/test/pixel3d/Pixel3DDemo.java
@@ -333,75 +333,90 @@ final class PolygonTools {
 		
 		// Loop through the rows.
 		for (int y = top; y < bottom; y++) {
-			// Build a list of nodes.
-			int nodes = 0;
+			final int nodeCount = buildNodeList(nodeX, nodeZ, y, vertices);
 			
-			for (int i = 0, j = vertices.length - 3; i < vertices.length; j = i, i += 3) {
-				final int y1 = (int) vertices[i + Y];
-				final int y2 = (int) vertices[j + Y];
-				final int dy = y2 - y1;
-				
-				if (y1 < y && y <= y2 || y2 < y && y <= y1) {
-					final int x1 = (int) vertices[i + X];
-					final int x2 = (int) vertices[j + X];
-					final int dx = x2 - x1;
-					nodeX[nodes] = (int) (x1 + (double) (y - y1) * dx / dy);
-					
-					final double z1 = vertices[i + Z];
-					final double z2 = vertices[j + Z];
-					final double dz = z2 - z1;
-					nodeZ[nodes] = z1 + (y - y1) * dz / dy;
-					
-					++nodes;
-				}
+			sortNodes(nodeX, nodeZ, nodeCount);
+			fillPixelsBetweenNodePairs(nodeX, nodeZ, nodeCount, y, clipLeft, clipRight, processor);
+		}
+	}
+	
+	public static final void fillPixelsBetweenNodePairs(final int[] nodeX,
+			final double[] nodeZ, final int nodeCount, final int y,
+			final int clipLeft, final int clipRight, final Processor processor) {
+		// Fill the pixels between node pairs.
+		for (int i = 0; i < nodeCount; i += 2) {
+			int x1 = nodeX[i];
+			
+			if (clipRight <= x1) {
+				break;
 			}
 			
-			// Sort the nodes, via a simple “Bubble” sort.
-			for (int i = 0; i < nodes - 1;) {
-				if (nodeX[i] > nodeX[i + 1]) {
-					swap(nodeX, i, i + 1);
-					swap(nodeZ, i, i + 1);
-					
-					if (i != 0) {
-						--i;
-					}
-				} else {
-					++i;
+			int x2 = nodeX[i + 1];
+			
+			if (clipLeft < x2) {
+				if (x1 < clipLeft) {
+					x1 = clipLeft;
+				}
+				
+				if (clipRight < x2) {
+					x2 = clipRight;
+				}
+				
+				final int dx = x2 - x1;
+				final double z1 = nodeZ[i];
+				final double z2 = nodeZ[i + 1];
+				final double dz = z2 - z1;
+				
+				for (int x = x1; x < x2; x++) {
+					processor.pixel(x, y, z1 + (x - x1) * dz / dx);
 				}
 			}
+		}
+	}
+	
+	public static final int buildNodeList(final int[] nodeX, final double[] nodeZ,
+			final int y, final double[] vertices) {
+		int result = 0;
+		
+		for (int i = 0, j = vertices.length - 3; i < vertices.length; j = i, i += 3) {
+			final int y1 = (int) vertices[i + Y];
+			final int y2 = (int) vertices[j + Y];
+			final int dy = y2 - y1;
 			
-			if ((nodes & 1) != 0) {
-				System.err.println(debug(DEBUG_STACK_OFFSET, "Internal error detected"));
+			if (y1 < y && y <= y2 || y2 < y && y <= y1) {
+				final int x1 = (int) vertices[i + X];
+				final int x2 = (int) vertices[j + X];
+				final int dx = x2 - x1;
+				nodeX[result] = (int) (x1 + (double) (y - y1) * dx / dy);
+				
+				final double z1 = vertices[i + Z];
+				final double z2 = vertices[j + Z];
+				final double dz = z2 - z1;
+				nodeZ[result] = z1 + (y - y1) * dz / dy;
+				
+				++result;
 			}
-			
-			// Fill the pixels between node pairs.
-			for (int i = 0; i < nodes; i += 2) {
-				int x1 = nodeX[i];
+		}
+		
+		if ((result & 1) != 0) {
+			System.err.println(debug(DEBUG_STACK_OFFSET, "Internal error detected"));
+		}
+		
+		return result;
+	}
+	
+	public static final void sortNodes(final int[] nodeX, final double[] nodeZ, final int nodeCount) {
+		// Sort the nodes, via a simple “Bubble” sort.
+		for (int i = 0; i < nodeCount - 1;) {
+			if (nodeX[i] > nodeX[i + 1]) {
+				swap(nodeX, i, i + 1);
+				swap(nodeZ, i, i + 1);
 				
-				if (clipRight <= x1) {
-					break;
+				if (i != 0) {
+					--i;
 				}
-				
-				int x2 = nodeX[i + 1];
-				
-				if (clipLeft < x2) {
-					if (x1 < clipLeft) {
-						x1 = clipLeft;
-					}
-					
-					if (clipRight < x2) {
-						x2 = clipRight;
-					}
-					
-					final int dx = x2 - x1;
-					final double z1 = nodeZ[i];
-					final double z2 = nodeZ[i + 1];
-					final double dz = z2 - z1;
-					
-					for (int x = x1; x < x2; x++) {
-						processor.pixel(x, y, z1 + (x - x1) * dz / dx);
-					}
-				}
+			} else {
+				++i;
 			}
 		}
 	}
