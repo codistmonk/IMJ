c4dc77be5ae89f161efc0eeb07b6b5d31a66a8f1
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2014-12-06 11:58:46 +0000

    [IMJ][imj3] Updated VisualSegmentation.

--- IMJ/src/imj3/draft/VisualSegmentation.java
+++ IMJ/src/imj3/draft/VisualSegmentation.java
@@ -2,18 +2,32 @@ package imj3.draft;
 
 import static net.sourceforge.aprog.swing.SwingTools.scrollable;
 import static net.sourceforge.aprog.tools.Tools.cast;
+
+import imj2.pixel3d.MouseHandler;
+
 import imj3.tools.AwtImage2D;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Window;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.prefs.Preferences;
 
 import javax.swing.ImageIcon;
@@ -21,9 +35,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 import javax.swing.WindowConstants;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
@@ -32,6 +48,7 @@ import javax.swing.tree.TreePath;
 import net.sourceforge.aprog.swing.SwingTools;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
 import net.sourceforge.aprog.tools.Pair;
+import net.sourceforge.aprog.tools.Tools;
 
 /**
  * @author codistmonk (creation 2014-12-03)
@@ -43,6 +60,17 @@ public final class VisualSegmentation {
 	}
 	
 	static final Preferences preferences = Preferences.userNodeForPackage(VisualSegmentation.class);
+	
+	static final Map<Object, Map<String, Object>> sharedProperties = new WeakHashMap<>();
+	
+	public static final void setSharedProperty(final Object object, final String key, final Object value) {
+		sharedProperties.computeIfAbsent(object, o -> new HashMap<>()).put(key, value);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static final <T> T getSharedProperty(final Object object, final String key) {
+		return (T) sharedProperties.getOrDefault(object, Collections.emptyMap()).get(key);
+	}
 	
 	/**
 	 * @param commandLineArguments
@@ -59,6 +87,8 @@ public final class VisualSegmentation {
 			mainFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			mainFrame.setPreferredSize(new Dimension(512, 512));
 			mainFrame.add(scrollable(tree), BorderLayout.WEST);
+			
+			setSharedProperty(mainFrame, "tree", tree);
 			
 			mainFrame.setDropTarget(new DropTarget() {
 				
@@ -87,11 +117,60 @@ public final class VisualSegmentation {
 					setView(mainFrame, view, new File(filePath));
 				}
 			}
+			
+			repeat(mainFrame, 30_000, e -> {
+				final Serializable model = (Serializable) tree.getModel();
+				
+				synchronized (model) {
+					Tools.writeObject(model, "palette.jo");
+				}
+			});
 		});
 	}
 	
+	public static final void repeat(final Window window, final int delay, final ActionListener action) {
+		final Timer timer = new Timer(delay, action) {
+			
+			@Override
+			public final void stop() {
+				try {
+					action.actionPerformed(null);
+				} finally {
+					super.stop();
+				}
+			}
+			
+			/**
+			 * {@value}.
+			 */
+			private static final long serialVersionUID = 4028342893634389147L;
+			
+		};
+		
+		timer.setRepeats(true);
+		
+		window.addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public final void windowClosing(final WindowEvent event) {
+				timer.stop();
+			}
+			
+		});
+	}
+	
+	public static final DefaultTreeModel loadModel() {
+		try {
+			return Tools.readObject("palette.jo");
+		} catch (final Exception exception) {
+			Tools.debugError(exception);
+		}
+		
+		return new DefaultTreeModel(new PaletteRoot());
+	}
+	
 	public static final JTree newPaletteTree() {
-		final DefaultTreeModel treeModel = new DefaultTreeModel(new PaletteRoot());
+		final DefaultTreeModel treeModel = loadModel();
 		final JTree result = new JTree(treeModel);
 		
 		result.addMouseListener(new MouseAdapter() {
@@ -211,7 +290,47 @@ public final class VisualSegmentation {
 	}
 	
 	public static final void setView(final JFrame mainFrame, final Component[] view, final File file) {
-		setView(mainFrame, view, scrollable(new JLabel(new ImageIcon(AwtImage2D.awtRead(file.getPath())))), file.getName());
+		final BufferedImage image = AwtImage2D.awtRead(file.getPath());
+		final JLabel newView = new JLabel(new ImageIcon(image));
+		
+		new MouseHandler(null) {
+			
+			@Override
+			public final void mouseClicked(final MouseEvent event) {
+				if (event.getClickCount() == 2) {
+					final JTree tree = getSharedProperty(mainFrame, "tree");
+					final TreePath selectionPath = tree.getSelectionPath();
+					
+					if (selectionPath != null) {
+						final PalettePrototype node = cast(PalettePrototype.class, selectionPath.getLastPathComponent());
+						
+						if (node != null) {
+							node.setUserObject(new Color(image.getRGB(event.getX(), event.getY())));
+							tree.repaint();
+						}
+					}
+				}
+			}
+			
+			/**
+			 * {@value}.
+			 */
+			private static final long serialVersionUID = 6325908040300578842L;
+			
+		}.addTo(newView);
+		
+		setView(mainFrame, view, scrollable(center(newView)), file.getName());
+	}
+	
+	public static final JPanel center(final Component component) {
+		final JPanel result = new JPanel(new GridBagLayout());
+		final GridBagConstraints c = new GridBagConstraints();
+		
+		c.anchor = GridBagConstraints.CENTER;
+		
+		result.add(component, c);
+		
+		return result;
 	}
 	
 	/**
