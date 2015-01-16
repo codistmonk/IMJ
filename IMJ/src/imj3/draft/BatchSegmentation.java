9d973198f5a004f9268e063f535802de752e1db0
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-01-16 05:28:00 +0000

    [IMJ][imj3] Started work on new BatchSegmentation.

--- IMJ/src/imj3/draft/BatchSegmentation.java
+++ IMJ/src/imj3/draft/BatchSegmentation.java
@@ -1,19 +1,21 @@
 package imj3.draft;
 
-import static imj3.core.Channels.Predefined.*;
-import static java.lang.Math.abs;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-import static net.sourceforge.aprog.tools.Tools.baseName;
-import imj3.tools.AwtImage2D;
-import imj3.tools.IMJTools;
+import static imj3.draft.VisualSegmentation.extractCells;
+import static imj3.draft.VisualSegmentation.newMaskFor;
+import static net.sourceforge.aprog.tools.Tools.getResourceAsStream;
+import imj2.draft.PaletteBasedHistograms;
+import imj2.tools.Canvas;
+import imj3.draft.VisualSegmentation.PaletteRoot;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.imageio.ImageIO;
 
@@ -23,7 +25,7 @@ import net.sourceforge.aprog.tools.TicToc;
 import net.sourceforge.aprog.tools.Tools;
 
 /**
- * @author codistmonk (creation 2014-12-03)
+ * @author codistmonk (creation 2015-01-15)
  */
 public final class BatchSegmentation {
 	
@@ -34,118 +36,91 @@ public final class BatchSegmentation {
 	/**
 	 * @param commandLineArguments
 	 * <br>Must not be null
+	 * @throws IOException
 	 */
-	public static final void main(final String[] commandLineArguments) {
-		IMJTools.toneDownBioFormatsLogger();
+	public static final void main(final String[] commandLineArguments) throws IOException {
 		final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser(commandLineArguments);
 		final File root = new File(arguments.get("in", ""));
+		final File outputRoot = new File(root, "results_ga_" + new SimpleDateFormat("YYYYMMDD", Locale.ENGLISH).format(new Date()));
+		final File referenceRoot = new File(root, arguments.get("reference", ""));
+		final PaletteRoot palette = VisualSegmentation.fromXML(getResourceAsStream(arguments.get("palette", "")));
 		final TicToc timer = new TicToc();
-		final AtomicInteger fileCount = new AtomicInteger();
+		
+		outputRoot.mkdir();
 		
 		for (final File file : root.listFiles()) {
 			final String fileName = file.getName();
-			if (fileName.endsWith(".png")) {
-				final String baseName = baseName(fileName);
-				final String maskName = baseName + "_mask.png";
-				final String resultName = baseName + "_segmented.png";
-				final File maskFile = new File(file.getParent(), maskName);
-				final File resultFile = new File(file.getParent(), resultName);
-				
-				if (maskFile.exists()/* && !resultFile.exists()*/) {
-					Tools.debugPrint(file);
-					
-					fileCount.incrementAndGet();
-					
-					timer.tic();
-					process(file, maskFile, resultFile);
-					Tools.debugPrint(timer.toc());
-					
-					break;
-				}
-			}
-		}
-		
-		Tools.debugPrint(fileCount, timer.getTotalTime());
-	}
-	
-	public static final void process(final File file, final File maskFile, final File resultFile) {
-		final AwtImage2D image = new AwtImage2D(file.getPath());
-		final AwtImage2D result = new AwtImage2D(file.getPath() + "_segmented", image.getWidth(), image.getHeight());
-		final AwtImage2D mask = new AwtImage2D(maskFile.getPath());
-		final AtomicInteger selectedPixelCount = new AtomicInteger();
-		final int imageWidth = image.getWidth();
-		final int imageHeight = image.getHeight();
-		final int windowSize = 11;
-		final int m = windowSize * windowSize;
-		final int maximumError = 32;
-		final Map<Integer, AtomicInteger> histogram = new HashMap<>();
-		
-		Tools.debugPrint(imageWidth, imageHeight);
-		
-		image.forEachPixel((x, y) -> {
-			if ((mask.getPixelValue(x, y) & 1) != 0) {
-				selectedPixelCount.incrementAndGet();
-				
-				final long pixelRGB = image.getPixelValue(x, y);
-				final int left = max(0, x - windowSize / 2);
-				final int right = min(left + windowSize, imageWidth) - 1;
-				final int top = max(0, y - windowSize / 2);
-				final int bottom = min(top + windowSize, imageHeight) - 1;
-				int r = 0;
-				int g = 0;
-				int b = 0;
-				int n = 0;
-				
-				for (int yy = top; yy <= bottom; ++yy) {
-					for (int xx = left; xx <= right; ++xx) {
-						final long neighborRGB = image.getPixelValue(xx, yy);
-						final int d = rgbRelativeD1(pixelRGB, neighborRGB);
-						
-						if (d <= maximumError) {
-							r += red8(neighborRGB);
-							g += green8(neighborRGB);
-							b += blue8(neighborRGB);
-							++n;
-						}
-					}
-				}
-				
-				r /= n;
-				g /= n;
-				b /= n;
-				final int d = 255 * n / m;
-				
-				{
-					final int key = a8r8g8b8(0xFF, r, g, b);
-					histogram.computeIfAbsent(key, k -> new AtomicInteger()).incrementAndGet();
-				}
-				
-				result.setPixelValue(x, y, a8r8g8b8(0xFF, r, g, b));
-			}
 			
-			return true;
-		});
-		
-		Tools.debugPrint(histogram.size());
-		
-		try {
-			Tools.debugPrint(resultFile);
-			ImageIO.write(result.getSource(), "png", resultFile);
-		} catch (final IOException exception) {
-			throw new UncheckedIOException(exception);
+			if (fileName.endsWith("lod6.jpg")) {
+				Tools.debugPrint(fileName);
+				
+				final String baseName = Tools.baseName(fileName);
+				final File segmentedFile = new File(outputRoot, baseName + "_segmented.png");
+				final File segmentsFile = new File(outputRoot, baseName + "_segments.png");
+				final BufferedImage segments;
+				
+				timer.tic();
+				
+				if (!segmentsFile.exists()) {
+					final BufferedImage image = ImageIO.read(file);
+					final BufferedImage mask = newMaskFor(image);
+					final Canvas labels = new Canvas().setFormat(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+					segments = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+					
+					extractCells(file, image, mask, labels, segments, palette);
+					VisualSegmentation.outlineSegments(segments, labels.getImage(), mask, image);
+					
+					Tools.debugPrint("Writing", segmentedFile);
+					ImageIO.write(image, "png", segmentedFile);
+					Tools.debugPrint("Writing", segmentsFile);
+					ImageIO.write(segments, "png", segmentsFile);
+				} else {
+					segments = ImageIO.read(segmentsFile);
+				}
+				
+				final File referenceFile = new File(referenceRoot,
+						referenceRoot.getName() + "_" + baseName.substring(0, baseName.length() - "_lod6".length()) + ".png");
+				
+				if (referenceFile.exists()) {
+					Tools.debugPrint(referenceFile);
+					
+					final BufferedImage reference = ImageIO.read(referenceFile);
+					final AtomicLong tp = new AtomicLong();
+					final AtomicLong fp = new AtomicLong();
+					final AtomicLong tn = new AtomicLong();
+					final AtomicLong fn = new AtomicLong();
+					
+					PaletteBasedHistograms.forEachPixelIn(reference, (x, y) -> {
+						final int xx = x * segments.getWidth() / reference.getWidth();
+						final int yy = y * segments.getHeight() / reference.getHeight();
+						final boolean prediction = segments.getRGB(xx, yy) != 0;
+						final boolean truth = (reference.getRGB(x, y) & 0x00FFFFFF) != 0;
+						
+						if (prediction) {
+							if (truth) {
+								tp.incrementAndGet();
+							} else {
+								fp.incrementAndGet();
+							}
+						} else {
+							if (truth) {
+								fn.incrementAndGet();
+							} else {
+								tn.incrementAndGet();
+							}
+						}
+						
+						return true;
+					});
+					
+					final double f1 = 2.0 * tp.get() / (2.0 * tp.get() + fp.get() + fn.get());
+					
+					Tools.debugPrint(tp, fp, tn, fn, f1);
+				}
+				
+				Tools.debugPrint(timer.toc());
+			}
 		}
-	}
-	
-	public static final int rgbRelativeD1(final long rgb1, final long rgb2) {
-		final int r1 = red8(rgb1);
-		final int g1 = green8(rgb1);
-		final int b1 = blue8(rgb1);
-		final int maxD = max(r1, 255 - r1) + max(g1, 255 - g1) + max(b1, 255 - b1);
-		final int r2 = red8(rgb2);
-		final int g2 = green8(rgb2);
-		final int b2 = blue8(rgb2);
-		
-		return 255 * (abs(r1 - r2) + abs(g1 - g2) + abs(b1 - b2)) / maxD;
 	}
 	
 }
