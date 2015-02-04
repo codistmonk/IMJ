73d67c7d458b3c0813f983ebe7cb75e21a03a1ee
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-04 11:13:39 +0000

    [IMJ][imj3] Refactoring.

---
+++ trunk/IMJ/src/imj3/draft/segmentation2/RandomPrototypeSource.java
@@ -0,0 +1,75 @@
+package imj3.draft.segmentation2;
+
+import imj3.draft.segmentation2.NearestNeighborClassifier.Prototype;
+
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * @author codistmonk (creation 2015-02-04)
+ */
+public final class RandomPrototypeSource implements DataSource<Prototype> {
+	
+	private final int dimension;
+	
+	private final int size;
+	
+	private final long seed;
+	
+	public RandomPrototypeSource(final int dimension, final int size, final long seed) {
+		this.dimension = dimension;
+		this.size = size;
+		this.seed = seed;
+	}
+	
+	public final long getSeed() {
+		return this.seed;
+	}
+	
+	@Override
+	public final Iterator<Classification<Prototype>> iterator() {
+		final int d = this.getDimension();
+		
+		return new Iterator<Classification<Prototype>>() {
+			
+			private final Random random = new Random(RandomPrototypeSource.this.getSeed());
+			
+			private final double[] datum = new double[d];
+			
+			private final Classification<Prototype> result = new Classification<>(
+					this.datum, new Prototype(this.datum), 0.0);
+			
+			private int i = 0;
+			
+			@Override
+			public final boolean hasNext() {
+				return this.i < RandomPrototypeSource.this.size();
+			}
+			
+			@Override
+			public final Classification<Prototype> next() {
+				++this.i;
+				
+				for (int i = 0; i < d; ++i) {
+					this.datum[i] = this.random.nextDouble();
+				}
+				
+				return this.result;
+			}
+			
+		};
+	}
+	
+	@Override
+	public final int getDimension() {
+		return this.dimension;
+	}
+	
+	@Override
+	public final int size() {
+		return this.size;
+	}
+	
+	private static final long serialVersionUID = -5303911125576388280L;
+	
+}
