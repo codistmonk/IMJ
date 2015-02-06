42c4cc7c9aab147e5690bbc6abba429af02677d2
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-06 16:05:12 +0000

    [IMJ][imj3] Refactoring.

--- IMJ/src/imj3/draft/segmentation2/Analyze.java
+++ IMJ/src/imj3/draft/segmentation2/Analyze.java
@@ -2,27 +2,25 @@ package imj3.draft.segmentation2;
 
 import static imj3.draft.segmentation2.DataSourceArrayProperty.CLASS;
 import static imj3.draft.segmentation2.DataSourceArrayProperty.INPUT;
+
 import imj3.core.Channels;
 import imj3.draft.machinelearning.Classification;
-import imj3.draft.machinelearning.Classifier;
-import imj3.draft.machinelearning.ClassifierClass;
 import imj3.draft.machinelearning.DataSource;
 import imj3.draft.machinelearning.KMeansClustering;
+import imj3.draft.machinelearning.LinearTransform;
 import imj3.draft.machinelearning.Measure;
 import imj3.draft.machinelearning.NearestNeighborClassifier;
 import imj3.draft.machinelearning.StreamingClustering;
+import imj3.draft.machinelearning.LinearTransform.Transformed;
 import imj3.draft.machinelearning.NearestNeighborClassifier.Prototype;
-import imj3.draft.segmentation2.Analyze.LinearTransform.Transformed;
 import imj3.tools.AwtImage2D;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.Arrays;
 
 import net.sourceforge.aprog.swing.SwingTools;
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
-import net.sourceforge.aprog.tools.Tools;
 
 /**
  * @author codistmonk (creation 2015-02-06)
@@ -126,95 +124,6 @@ public final class Analyze {
 	
 	public static final int int8(final double value0255) {
 		return ((int) value0255) & 0xFF;
-	}
-	
-	/**
-	 * @author codistmonk (creation 2015-02-06)
-	 */
-	public static final class LinearTransform implements Classifier<LinearTransform.Transformed> {
-		
-		private final double[][] matrix;
-		
-		private final ClassifierClass.Measure<Transformed> transformedMeasure;
-		
-		public LinearTransform(final Measure measure, double[][] matrix) {
-			this.matrix = matrix;
-			this.transformedMeasure = new ClassifierClass.Measure.Default<>(measure);
-		}
-		
-		public final double[][] getMatrix() {
-			return this.matrix;
-		}
-		
-		public final int getMatrixRowCount() {
-			return this.getMatrix().length;
-		}
-		
-		public final int getMatrixColumnCount() {
-			return this.getMatrix()[0].length;
-		}
-		
-		@Override
-		public final ClassifierClass.Measure<Transformed> getClassMeasure() {
-			return this.transformedMeasure;
-		}
-		
-		@Override
-		public final Classification<Transformed> classify(final double... input) {
-			final int n = this.getMatrixRowCount();
-			final double[] datum = new double[n];
-			
-			for (int i = 0; i < n; ++i) {
-				datum[i] = dot(this.getMatrix()[i], input);
-			}
-			
-			return new Classification<>(input, new Transformed(datum), 0.0);
-		}
-		
-		@Override
-		public final int getClassDimension(final int inputDimension) {
-			if (inputDimension == this.getMatrixColumnCount()) {
-				return this.getMatrixRowCount();
-			}
-			
-			Tools.debugPrint(inputDimension, this.getMatrixRowCount(), this.getMatrixColumnCount());
-			
-			throw new IllegalArgumentException();
-		}
-		
-		private static final long serialVersionUID = -1314577713442253655L;
-		
-		public static final double dot(final double[] v1, final double[] v2) {
-			final int n = v1.length;
-			double result = 0.0;
-			
-			for (int i = 0; i < n; ++i) {
-				result += v1[i] * v2[i];
-			}
-			
-			return result;
-		}
-		
-		/**
-		 * @author codistmonk (creation 2015-02-06)
-		 */
-		public static final class Transformed implements ClassifierClass {
-			
-			private final double[] datum;
-			
-			public Transformed(final double[] datum) {
-				this.datum = datum;
-			}
-			
-			@Override
-			public final double[] toArray() {
-				return this.datum;
-			}
-			
-			private static final long serialVersionUID = 5806137995866347277L;
-			
-		}
-		
 	}
 	
 }
