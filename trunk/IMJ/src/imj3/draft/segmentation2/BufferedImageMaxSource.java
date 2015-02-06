86cc30ea5de7c6e393cbe44558e9b701440c12b2
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-06 15:19:28 +0000

    [IMJ][imj3] Refactoring.

--- trunk/IMJ/src/imj3/draft/segmentation2/BufferedImageMaxSource.java
+++ trunk/IMJ/src/imj3/draft/segmentation2/BufferedImageMaxSource.java
@@ -3,42 +3,39 @@ package imj3.draft.segmentation2;
 import static imj3.core.Channels.Predefined.blue8;
 import static imj3.core.Channels.Predefined.green8;
 import static imj3.core.Channels.Predefined.red8;
-import imj3.draft.machinelearning.KMeansClustering;
+import static java.lang.Math.max;
 
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 
 /**
  * @author codistmonk (creation 2015-02-06)
  */
-public final class Mean extends BufferedImagePrototypeSource {
+public final class BufferedImageMaxSource extends BufferedImagePrototypeSource {
 	
-	public Mean(final BufferedImage image, final int patchSize) {
+	public BufferedImageMaxSource(final BufferedImage image, final int patchSize) {
 		super(image, patchSize);
 	}
 	
-	public Mean(final BufferedImage image, final int patchSize, final int patchSparsity, final int stride) {
+	public BufferedImageMaxSource(final BufferedImage image, final int patchSize, final int patchSparsity, final int stride) {
 		super(image, patchSize, patchSparsity, stride);
 	}
 	
 	@Override
 	public final int getInputDimension() {
-		return 3;
+		return 1;
 	}
 	
 	@Override
 	protected final void convert(final int x, final int y, final int[] patchValues, final double[] result) {
-		Arrays.fill(result, 0.0);
+		double max = 0.0;
 		
 		for (final int value : patchValues) {
-			result[0] += red8(value);
-			result[1] += green8(value);
-			result[2] += blue8(value);
+			max = max(max, max(red8(value), max(green8(value), blue8(value))));
 		}
 		
-		KMeansClustering.divide(result, patchValues.length);
+		result[0] = max;
 	}
 	
-	private static final long serialVersionUID = 3938160512172714562L;
+	private static final long serialVersionUID = -3551891713169505385L;
 	
 }
