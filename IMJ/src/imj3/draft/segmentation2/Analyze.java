459a35eff99bbebac3ef942d6e26df55701e7962
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-06 16:03:13 +0000

    [IMJ][imj3][segmentation2] Updated Analyze.

--- IMJ/src/imj3/draft/segmentation2/Analyze.java
+++ IMJ/src/imj3/draft/segmentation2/Analyze.java
@@ -4,18 +4,25 @@ import static imj3.draft.segmentation2.DataSourceArrayProperty.CLASS;
 import static imj3.draft.segmentation2.DataSourceArrayProperty.INPUT;
 import imj3.core.Channels;
 import imj3.draft.machinelearning.Classification;
+import imj3.draft.machinelearning.Classifier;
+import imj3.draft.machinelearning.ClassifierClass;
+import imj3.draft.machinelearning.DataSource;
 import imj3.draft.machinelearning.KMeansClustering;
 import imj3.draft.machinelearning.Measure;
+import imj3.draft.machinelearning.NearestNeighborClassifier;
 import imj3.draft.machinelearning.StreamingClustering;
 import imj3.draft.machinelearning.NearestNeighborClassifier.Prototype;
+import imj3.draft.segmentation2.Analyze.LinearTransform.Transformed;
 import imj3.tools.AwtImage2D;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Arrays;
 
 import net.sourceforge.aprog.swing.SwingTools;
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
+import net.sourceforge.aprog.tools.Tools;
 
 /**
  * @author codistmonk (creation 2015-02-06)
@@ -39,25 +46,47 @@ public final class Analyze {
 		
 		if (false) {
 			final BufferedImageMeanSource mean = new BufferedImageMeanSource(image, 2, 1, 2);
-			SwingTools.show(convert(mean, INPUT), "Mean", false);
-			SwingTools.show(convert(new BufferedImageMaxSource(image, 2, 1, 2), INPUT), "Max", false);
+			SwingTools.show(awtImage(mean, INPUT), "Mean", false);
+			SwingTools.show(awtImage(new BufferedImageMaxSource(image, 2, 1, 2), INPUT), "Max", false);
 			
-			SwingTools.show(convert(new ClassifiedImageDataSource<>(mean, new StreamingClustering(Measure.Predefined.L1_ES, 8).cluster(mean)), CLASS), "Indirect (streaming)", false);
-			SwingTools.show(convert(new ClassifiedImageDataSource<>(mean, new KMeansClustering(Measure.Predefined.L1_ES, 8, 6).cluster(mean)), CLASS), "Indirect (k-means)", false);
+			SwingTools.show(awtImage(new ClassifiedImageDataSource<>(mean, new StreamingClustering(Measure.Predefined.L1_ES, 8).cluster(mean)), CLASS), "Indirect (streaming)", false);
+			SwingTools.show(awtImage(new ClassifiedImageDataSource<>(mean, new KMeansClustering(Measure.Predefined.L1_ES, 8, 6).cluster(mean)), CLASS), "Indirect (k-means)", false);
 		}
 		
 		if (true) {
-			final BufferedImageRawSource source = new BufferedImageRawSource(image, 1);
+			final BufferedImageRawSource source = new BufferedImageRawSource(image, 4);
+			final DataSource<Prototype> trainingSet = source;
+			final NearestNeighborClassifier quantizer = new StreamingClustering(Measure.Predefined.L1_ES, 8).cluster(trainingSet);
+			final ClassifiedImageDataSource<Prototype, Prototype> quantized = new ClassifiedImageDataSource<>(source, quantizer);
+			final LinearTransform rgbRenderer = new LinearTransform(Measure.Predefined.L1_ES, newRGBRenderingMatrix(source.getPatchPixelCount()));
+			final ClassifiedImageDataSource<Prototype, Transformed> rendered = new ClassifiedImageDataSource<>(quantized, rgbRenderer);
 			
-			SwingTools.show(convert(new ClassifiedImageDataSource<>(source, new StreamingClustering(Measure.Predefined.L1_ES, 8).cluster(source)), CLASS), "Indirect (streaming)", false);
+			SwingTools.show(awtImage(rendered, CLASS), "Quantized (streaming) -> rendered", false);
 		}
 	}
 	
-	public static final BufferedImage convert(final ImageDataSource<Prototype> source, final DataSourceArrayProperty property) {
-		return convert(source, property, null);
+	public static final double[][] newRGBRenderingMatrix(final int inputPatchPixelCount) {
+		final double[][] result = new double[3][];
+		final int n = 3 * inputPatchPixelCount;
+		
+		for (int i = 0; i < 3; ++i) {
+			final double[] row = new double[n];
+			
+			for (int j = i; j < n; j += 3) {
+				row[j] = 1.0 / inputPatchPixelCount;
+			}
+			
+			result[i] = row;
+		}
+		
+		return result;
 	}
 	
-	public static final BufferedImage convert(final ImageDataSource<Prototype> source, final DataSourceArrayProperty property, final BufferedImage result) {
+	public static final BufferedImage awtImage(final ImageDataSource<?> source, final DataSourceArrayProperty property) {
+		return awtImage(source, property, null);
+	}
+	
+	public static final BufferedImage awtImage(final ImageDataSource<?> source, final DataSourceArrayProperty property, final BufferedImage result) {
 		final int dimension = property.getDimension(source);
 		
 		if (dimension != 1 && dimension != 3) {
@@ -71,7 +100,7 @@ public final class Analyze {
 		
 		int pixel = 0;
 		
-		for (final Classification<Prototype> c : source) {
+		for (final Classification<?> c : source) {
 			final int x = pixel % width;
 			final int y = pixel / width;
 			final double[] input = property.getArray(c);
@@ -97,6 +126,95 @@ public final class Analyze {
 	
 	public static final int int8(final double value0255) {
 		return ((int) value0255) & 0xFF;
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-02-06)
+	 */
+	public static final class LinearTransform implements Classifier<LinearTransform.Transformed> {
+		
+		private final double[][] matrix;
+		
+		private final ClassifierClass.Measure<Transformed> transformedMeasure;
+		
+		public LinearTransform(final Measure measure, double[][] matrix) {
+			this.matrix = matrix;
+			this.transformedMeasure = new ClassifierClass.Measure.Default<>(measure);
+		}
+		
+		public final double[][] getMatrix() {
+			return this.matrix;
+		}
+		
+		public final int getMatrixRowCount() {
+			return this.getMatrix().length;
+		}
+		
+		public final int getMatrixColumnCount() {
+			return this.getMatrix()[0].length;
+		}
+		
+		@Override
+		public final ClassifierClass.Measure<Transformed> getClassMeasure() {
+			return this.transformedMeasure;
+		}
+		
+		@Override
+		public final Classification<Transformed> classify(final double... input) {
+			final int n = this.getMatrixRowCount();
+			final double[] datum = new double[n];
+			
+			for (int i = 0; i < n; ++i) {
+				datum[i] = dot(this.getMatrix()[i], input);
+			}
+			
+			return new Classification<>(input, new Transformed(datum), 0.0);
+		}
+		
+		@Override
+		public final int getClassDimension(final int inputDimension) {
+			if (inputDimension == this.getMatrixColumnCount()) {
+				return this.getMatrixRowCount();
+			}
+			
+			Tools.debugPrint(inputDimension, this.getMatrixRowCount(), this.getMatrixColumnCount());
+			
+			throw new IllegalArgumentException();
+		}
+		
+		private static final long serialVersionUID = -1314577713442253655L;
+		
+		public static final double dot(final double[] v1, final double[] v2) {
+			final int n = v1.length;
+			double result = 0.0;
+			
+			for (int i = 0; i < n; ++i) {
+				result += v1[i] * v2[i];
+			}
+			
+			return result;
+		}
+		
+		/**
+		 * @author codistmonk (creation 2015-02-06)
+		 */
+		public static final class Transformed implements ClassifierClass {
+			
+			private final double[] datum;
+			
+			public Transformed(final double[] datum) {
+				this.datum = datum;
+			}
+			
+			@Override
+			public final double[] toArray() {
+				return this.datum;
+			}
+			
+			private static final long serialVersionUID = 5806137995866347277L;
+			
+		}
+		
 	}
 	
 }
