3e12511bfc7175e9127d9fc60939787888316080
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-08 17:30:34 +0000

    [IMJ][imj3] Refactoring.

--- IMJ/src/imj3/draft/segmentation2/Analyze.java
+++ IMJ/src/imj3/draft/segmentation2/Analyze.java
@@ -1,10 +1,9 @@
 package imj3.draft.segmentation2;
 
-import static imj3.draft.segmentation2.DataSourceArrayProperty.CLASS;
-import static imj3.draft.segmentation2.DataSourceArrayProperty.INPUT;
 import imj3.core.Channels;
 import imj3.draft.machinelearning.Classification;
 import imj3.draft.machinelearning.ClassifiedDataSource;
+import imj3.draft.machinelearning.ClassifierClass;
 import imj3.draft.machinelearning.DataSource;
 import imj3.draft.machinelearning.KMeansClustering;
 import imj3.draft.machinelearning.LinearTransform;
@@ -12,16 +11,18 @@ import imj3.draft.machinelearning.Measure;
 import imj3.draft.machinelearning.NearestNeighborClassifier;
 import imj3.draft.machinelearning.NearestNeighborClustering;
 import imj3.draft.machinelearning.StreamingClustering;
-import imj3.draft.machinelearning.LinearTransform.Transformed;
 import imj3.draft.machinelearning.NearestNeighborClassifier.Prototype;
 import imj3.tools.AwtImage2D;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Iterator;
 
 import net.sourceforge.aprog.swing.SwingTools;
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
+import net.sourceforge.aprog.tools.TicToc;
+import net.sourceforge.aprog.tools.Tools;
 
 /**
  * @author codistmonk (creation 2015-02-06)
@@ -45,11 +46,11 @@ public final class Analyze {
 		
 		if (false) {
 			final BufferedImageMeanSource mean = new BufferedImageMeanSource(image, 2, 1, 2);
-			SwingTools.show(awtImage(mean, INPUT), "Mean", false);
-			SwingTools.show(awtImage(new BufferedImageMaxSource(image, 2, 1, 2), INPUT), "Max", false);
+			SwingTools.show(awtImage(mean), "Mean", false);
+			SwingTools.show(awtImage(new BufferedImageMaxSource(image, 2, 1, 2)), "Max", false);
 			
-			SwingTools.show(awtImage(new ClassifiedDataSource<>(mean, new StreamingClustering(Measure.Predefined.L1_ES, 8).cluster(mean)), CLASS), "Indirect (streaming)", false);
-			SwingTools.show(awtImage(new ClassifiedDataSource<>(mean, new KMeansClustering(Measure.Predefined.L1_ES, 8, 6).cluster(mean)), CLASS), "Indirect (k-means)", false);
+			SwingTools.show(awtImage(new ClassDataSource<>(new ClassifiedDataSource<>(mean, new StreamingClustering(Measure.Predefined.L1_ES, 8).cluster(mean)))), "Indirect (streaming)", false);
+			SwingTools.show(awtImage(new ClassDataSource<>(new ClassifiedDataSource<>(mean, new KMeansClustering(Measure.Predefined.L1_ES, 8, 6).cluster(mean)))), "Indirect (k-means)", false);
 		}
 		
 		if (true) {
@@ -61,9 +62,9 @@ public final class Analyze {
 			final NearestNeighborClassifier quantizer = clustering.cluster(trainingSet);
 			final DataSource<? extends ImageDataSource.Metadata, Prototype> quantized = new ClassifiedDataSource<>(source, quantizer);
 			final LinearTransform rgbRenderer = new LinearTransform(Measure.Predefined.L1_ES, newRGBRenderingMatrix(source.getMetadata().getPatchPixelCount()));
-			final DataSource<? extends ImageDataSource.Metadata, Transformed> rendered = new ClassifiedDataSource<>(quantized, rgbRenderer);
+			final DataSource<? extends ImageDataSource.Metadata, ?> rendered = new ClassifiedDataSource<>(quantized, rgbRenderer);
 			
-			SwingTools.show(awtImage(rendered, CLASS), clustering.getClass().getSimpleName() + " -> rendered", false);
+			SwingTools.show(awtImage(new ClassDataSource<>(rendered)), clustering.getClass().getSimpleName() + " -> rendered", false);
 		}
 	}
 	
@@ -84,12 +85,13 @@ public final class Analyze {
 		return result;
 	}
 	
-	public static final BufferedImage awtImage(final DataSource<? extends ImageDataSource.Metadata, ?> source, final DataSourceArrayProperty property) {
-		return awtImage(source, property, null);
+	public static final BufferedImage awtImage(final DataSource<? extends ImageDataSource.Metadata, ?> source) {
+		return awtImage(source, null);
 	}
 	
-	public static final BufferedImage awtImage(final DataSource<? extends ImageDataSource.Metadata, ?> source, final DataSourceArrayProperty property, final BufferedImage result) {
-		final int dimension = property.getDimension(source);
+	public static final BufferedImage awtImage(final DataSource<? extends ImageDataSource.Metadata, ?> source, final BufferedImage result) {
+		final TicToc timer = new TicToc();
+		final int dimension = source.getInputDimension();
 		
 		if (dimension != 1 && dimension != 3) {
 			throw new IllegalArgumentException();
@@ -105,13 +107,15 @@ public final class Analyze {
 		for (final Classification<?> c : source) {
 			final int x = pixel % width;
 			final int y = pixel / width;
-			final double[] input = property.getArray(c);
+			final double[] input = c.getInput();
 			final int rgb = dimension == 1 ? gray(input) : argb(input);
 			
 			actualResult.setRGB(x, y, rgb);
 			
 			++pixel;
 		}
+		
+		Tools.debugPrint("Awt image created in", timer.toc(), "ms");
 		
 		return actualResult;
 	}
@@ -128,6 +132,86 @@ public final class Analyze {
 	
 	public static final int int8(final double value0255) {
 		return ((int) value0255) & 0xFF;
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-02-08)
+	 * 
+	 * @param <M>
+	 * @param <C>
+	 */
+	public static final class ClassDataSource<M extends DataSource.Metadata> extends DataSource.Abstract<M, ClassifierClass> {
+		
+		private final DataSource<M, ClassifierClass> source;
+		
+		@SuppressWarnings("unchecked")
+		public ClassDataSource(final DataSource<M, ?> source) {
+			super(source.getMetadata());
+			this.source = (DataSource<M, ClassifierClass>) source;
+		}
+		
+		@Override
+		public final int getInputDimension() {
+			return this.getSource().getClassDimension();
+		}
+		
+		@Override
+		public final int getClassDimension() {
+			return this.getInputDimension();
+		}
+		
+		@Override
+		public final Iterator<Classification<ClassifierClass>> iterator() {
+			return new Iterator<Classification<ClassifierClass>>() {
+				
+				private final Iterator<Classification<ClassifierClass>> i = ClassDataSource.this.getSource().iterator();
+				
+				private final Classification<MutableClassifierClass> result = new Classification<>(null, new MutableClassifierClass(), 0.0);
+				
+				@Override
+				public final boolean hasNext() {
+					return this.i.hasNext();
+				}
+				
+				@SuppressWarnings("unchecked")
+				@Override
+				public final Classification<ClassifierClass> next() {
+					return (Classification) this.result.setInput(
+							this.result.getClassifierClass().setArray(
+									this.i.next().getClassifierClass().toArray()).toArray());
+				}
+				
+			};
+		}
+		
+		final DataSource<M, ClassifierClass> getSource() {
+			return this.source;
+		}
+		
+		private static final long serialVersionUID = -4926248507282321872L;
+		
+		/**
+		 * @author codistmonk (creation 2015-02-08)
+		 */
+		public static final class MutableClassifierClass implements ClassifierClass {
+			
+			private double[] array;
+			
+			public final MutableClassifierClass setArray(final double[] array) {
+				this.array = array;
+				
+				return this;
+			}
+			
+			@Override
+			public final double[] toArray() {
+				return this.array;
+			}
+			
+			private static final long serialVersionUID = 8563042210828121994L;
+			
+		}
+		
 	}
 	
 }
