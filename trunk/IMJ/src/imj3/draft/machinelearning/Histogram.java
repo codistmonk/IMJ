120a1fe1a7ae9f2347d36edfbf40aced4034e7c5
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-12 07:05:24 +0000

    [IMJ][imj3] Refactoring.

---
+++ trunk/IMJ/src/imj3/draft/machinelearning/Histogram.java
@@ -0,0 +1,91 @@
+package imj3.draft.machinelearning;
+
+import static java.lang.Math.rint;
+import imj2.tools.BitwiseQuantizationTest.DoubleArrayComparator;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author codistmonk (creation 2015-02-12)
+ */
+public final class Histogram implements Serializable {
+	
+	private final Map<double[], double[]> counts = new TreeMap<>(new DoubleArrayComparator());
+	
+	private long totalCount;
+	
+	public final Map<double[], double[]> getCounts() {
+		return this.counts;
+	}
+	
+	public final long getTotalCount() {
+		return this.totalCount;
+	}
+	
+	public final Histogram reset() {
+		this.getCounts().clear();
+		this.totalCount = 0L;
+		
+		return this;
+	}
+	
+	public final Histogram add(final double... input) {
+		++this.getCounts().computeIfAbsent(input, i -> new double[1])[0];
+		++this.totalCount;
+		
+		return this;
+	}
+	
+	public final Histogram add(final DataSource<?, ?> inputs) {
+		for (final Classification<?> classification : inputs) {
+			this.add(classification.getInput().clone());
+		}
+		
+		return this;
+	}
+	
+	public final Histogram normalize() {
+		final long n = this.getTotalCount();
+		
+		if (0L < n) {
+			for (final double[] count : this.getCounts().values()) {
+				count[0] /= n;
+			}
+		}
+		
+		return this;
+	}
+	
+	public final Histogram denormalize() {
+		final long n = this.getTotalCount();
+		
+		if (0L < n) {
+			for (final double[] count : this.getCounts().values()) {
+				count[0] = rint(count[0] * n);
+			}
+		}
+		
+		return this;
+	}
+	
+	public final double[] pack(final int binCount) {
+		final double[] result = new double[binCount];
+		
+		for (final Map.Entry<double[], double[]> entry : this.getCounts().entrySet()) {
+			final double[] key = entry.getKey();
+			
+			if (key.length != 1) {
+				throw new IllegalArgumentException();
+			}
+			
+			result[(int) key[0]] = entry.getValue()[0];
+		}
+		
+		return result;
+	}
+	
+	private static final long serialVersionUID = -4974336898629198663L;
+	
+}
--- trunk/IMJ/src/imj3/draft/processing/Analyze.java
+++ trunk/IMJ/src/imj3/draft/processing/Analyze.java
@@ -4,9 +4,6 @@ import static imj3.draft.machinelearning.ClassDataSource.classes;
 import static imj3.draft.machinelearning.Max.max;
 import static imj3.draft.machinelearning.Mean.mean;
 import static imj3.draft.processing.Image2DRawSource.raw;
-import static java.lang.Math.rint;
-
-import imj2.tools.BitwiseQuantizationTest.DoubleArrayComparator;
 
 import imj3.core.Channels;
 import imj3.draft.machinelearning.BufferedDataSource;
@@ -15,6 +12,7 @@ import imj3.draft.machinelearning.ClassifiedDataSource;
 import imj3.draft.machinelearning.Classifier;
 import imj3.draft.machinelearning.ClassifierClass;
 import imj3.draft.machinelearning.DataSource;
+import imj3.draft.machinelearning.Histogram;
 import imj3.draft.machinelearning.KMeansClustering;
 import imj3.draft.machinelearning.LinearTransform;
 import imj3.draft.machinelearning.Measure;
@@ -26,10 +24,7 @@ import imj3.draft.machinelearning.NearestNeighborClassifier.Prototype;
 import imj3.tools.AwtImage2D;
 
 import java.io.File;
-import java.io.Serializable;
-import java.util.Map;
 import java.util.Random;
-import java.util.TreeMap;
 
 import net.sourceforge.aprog.swing.SwingTools;
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
@@ -155,89 +150,6 @@ public final class Analyze {
 	
 	public static final int int8(final double value0255) {
 		return ((int) value0255) & 0xFF;
-	}
-	
-	/**
-	 * @author codistmonk (creation 2015-02-12)
-	 */
-	public static final class Histogram implements Serializable {
-		
-		private final Map<double[], double[]> counts = new TreeMap<>(new DoubleArrayComparator());
-		
-		private long totalCount;
-		
-		public final Map<double[], double[]> getCounts() {
-			return this.counts;
-		}
-		
-		public final long getTotalCount() {
-			return this.totalCount;
-		}
-		
-		public final Histogram reset() {
-			this.getCounts().clear();
-			this.totalCount = 0L;
-			
-			return this;
-		}
-		
-		public final Histogram add(final double... input) {
-			++this.getCounts().computeIfAbsent(input, i -> new double[1])[0];
-			++this.totalCount;
-			
-			return this;
-		}
-		
-		public final Histogram add(final DataSource<?, ?> inputs) {
-			for (final Classification<?> classification : inputs) {
-				this.add(classification.getInput().clone());
-			}
-			
-			return this;
-		}
-		
-		public final Histogram normalize() {
-			final long n = this.getTotalCount();
-			
-			if (0L < n) {
-				for (final double[] count : this.getCounts().values()) {
-					count[0] /= n;
-				}
-			}
-			
-			return this;
-		}
-		
-		public final Histogram denormalize() {
-			final long n = this.getTotalCount();
-			
-			if (0L < n) {
-				for (final double[] count : this.getCounts().values()) {
-					count[0] = rint(count[0] * n);
-				}
-			}
-			
-			return this;
-		}
-		
-		public final double[] pack(final int binCount) {
-			final double[] result = new double[binCount];
-			
-			for (final Map.Entry<double[], double[]> entry : this.getCounts().entrySet()) {
-				final double[] key = entry.getKey();
-				
-				if (key.length != 1) {
-					throw new IllegalArgumentException();
-				}
-				
-				result[(int) key[0]] = entry.getValue()[0];
-			}
-			
-			return result;
-		}
-		
-		private static final long serialVersionUID = -4974336898629198663L;
-		
 	}
 	
 }
