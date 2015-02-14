b16da2dbc78d5c3a4a29d4aa35aa26ba5f88d0a5
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-02-14 08:18:01 +0000

    [JOCLTest] Refactoring.

--- JOCLTest/src/jocltest/JOCLConvolution2D.java
+++ JOCLTest/src/jocltest/JOCLConvolution2D.java
@@ -12,15 +12,18 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import javax.imageio.ImageIO;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import org.jocl.CL;
-import org.jocl.Sizeof;
+import javax.imageio.ImageIO;
 
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
 import net.sourceforge.aprog.tools.Tools;
 import net.sourceforge.aprog.tools.MathTools.Statistics;
+
+import org.jocl.CL;
+import org.jocl.Sizeof;
 
 /**
  * @author codistmonk (creation 2014-12-07)
@@ -30,63 +33,22 @@ public final class JOCLConvolution2D {
 	private JOCLConvolution2D() {
 		throw new IllegalInstantiationException();
 	}
-	
-    public static final String PROGRAM_SOURCE =
-            "__kernel void "
-            + "convolution2D(__global const float * input,"
-            + "             __global const int * inputDimensions,"
-            + "             __global const int * stepping,"
-            + "             __global const float * convolutions,"
-            + "             __global const int * convolutionsDimensions,"
-            + "             __global float * output)"
-            + "{\n"
-            + "	const int gid = get_global_id(0);\n"
-            + "	const int inputChannels = inputDimensions[0];\n"
-            + "	const int inputWidth = inputDimensions[1];\n"
-            + "	const int inputHeight = inputDimensions[2];\n"
-            + "	const int inputPlaneSize = inputWidth * inputHeight;\n"
-            + "	const int sparsity = stepping[0];\n"
-            + "	const int stride = stepping[1];\n"
-            + "	const int convolutionCount = convolutionsDimensions[0];\n"
-            + "	const int convolutionWidth = convolutionsDimensions[1];\n"
-            + "	const int convolutionHeight = convolutionsDimensions[2];\n"
-            + "	const int convolutionSize = convolutionWidth * convolutionHeight * inputChannels;\n"
-            + "	const int x = stride * (gid % (inputWidth / stride));\n"
-            + "	const int y = stride * (gid / (inputWidth / stride));\n"
-            + "	const int virtualLeft = x + 1 - (convolutionWidth / 2);\n"
-            + "	const int virtualRight = virtualLeft + convolutionWidth;\n"
-            + "	const int virtualTop = y + 1 - (convolutionHeight / 2);\n"
-            + "	const int virtualBottom = virtualTop + convolutionHeight;\n"
-            + "	const int left = max(0, virtualLeft);\n"
-            + "	const int right = min(virtualRight, inputWidth) - 1;\n"
-            + "	const int top = max(0, virtualTop);\n"
-            + "	const int bottom = min(virtualBottom, inputHeight) - 1;\n"
-            + "	const int cleft = left - virtualLeft;\n"
-            + "	const int cright = (virtualRight - 1) - right;\n"
-            + "	const int ctop = top - virtualTop;\n"
-            + "	const int cbottom = (virtualBottom - 1) - bottom;\n"
-            + "	const int outputPlaneSize = (inputWidth / stride) * (inputHeight / stride);\n"
-            + "	for (int o = 0; o < convolutionCount; ++o)\n"
-            + "	{\n"
-            + "		output[outputPlaneSize * o + gid] = 0;\n"
-            + "	}\n"
-            + "	for (int yy = top, cy = ctop; yy <= bottom; yy += sparsity, ++cy)\n"
-            + "	{\n"
-            + "		for (int xx = left, cx = cleft; xx <= right; xx += sparsity, ++cx)\n"
-            + "		{\n"
-            + "			const int inputOffset = (yy * inputWidth + xx);\n"
-            + "			const int convolutionOffset = (cy * convolutionWidth + cx) * inputChannels;\n"
-            + "			for (int c = 0; c < inputChannels; ++c)\n"
-            + "			{\n"
-            + "				const float inputValue = input[inputPlaneSize * c + inputOffset];\n"
-            + "				for (int o = 0; o < convolutionCount; ++o)\n"
-            + "				{\n"
-            + "					output[outputPlaneSize * o + gid] += inputValue * convolutions[o * convolutionSize + convolutionOffset + c];\n"
-            + "				}\n"
-            + "			}\n"
-            + "		}\n"
-            + "	}\n"
-            + "}";
+    
+    public static final TemporaryObject<CLContext> temporaryContext = new TemporaryObject<CLContext>(2_000L) {
+    	
+		@Override
+		protected final CLContext newObject() {
+			return new CLContext(CL.CL_DEVICE_TYPE_GPU, 0);
+		}
+		
+		@Override
+		protected final void deleteObject(final CLContext context) {
+			context.release();
+		}
+		
+		private static final long serialVersionUID = -956670618965318967L;
+		
+	};
     
 	/**
 	 * @param commandLineArguments
@@ -130,27 +92,19 @@ public final class JOCLConvolution2D {
 		
 		CL.setExceptionsEnabled(true);
 		
-		final CLContext context = new CLContext(CL.CL_DEVICE_TYPE_GPU, 0);
-		final CLKernel kernel = context.createAndBuildProgram(PROGRAM_SOURCE)
-				.createKernel("convolution2D");
-		final int outputWidth = imageWidth / stride;
-		final int outputHeight = imageHeight / stride;
-		final int outputPixelCount = outputWidth * outputHeight;
-		final float[] output = new float[outputPixelCount * convolutionCount];
+		final Convolutions2D c = new Convolutions2D(temporaryContext.get());
 		
-		kernel.setArg(0, context.createInputBuffer(input));
-		kernel.setArg(1, context.createInputBuffer(channelCount, imageWidth, imageHeight));
-		kernel.setArg(2, context.createInputBuffer(1, stride));
-		kernel.setArg(3, context.createInputBuffer(convolutions));
-		kernel.setArg(4, context.createInputBuffer(convolutionCount, convolutionSize, convolutionSize));
-		kernel.setArg(5, context.createOutputBuffer(Sizeof.cl_float, output.length));
+		c.setInput(input).setInputDimensions(channelCount, imageWidth, imageHeight).setStepping(1, stride);
+		c.setConvolutions(convolutions).setConvolutionsDimensions(convolutionCount, convolutionSize, convolutionSize);
+		c.setOutput();
 		
 		Tools.debugPrint();
 		
-		kernel.enqueueNDRange(output.length / convolutionCount);
-		kernel.enqueueReadArg(5, output);
+		final float[] output = c.compute();
+		final int outputWidth = c.getOutputWidth();
+		final int outputHeight = c.getOutputHeight();
+		final int outputPixelCount = outputWidth * outputHeight;
 		
-		context.release();
 		Tools.debugPrint();
 		
 		final BufferedImage outputImage = new BufferedImage(outputWidth, outputHeight, BufferedImage.TYPE_INT_ARGB);
@@ -323,6 +277,246 @@ public final class JOCLConvolution2D {
 		public static final Denormalize denormalizeTo(final int minimum, final int maximum) {
 			return new Denormalize(minimum, maximum);
 		}
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-02-14)
+	 * @param <T>
+	 */
+	public static abstract class TemporaryObject<T> implements Serializable {
+		
+		private final long milliseconds;
+		
+		private long lastAccess;
+		
+		private Timer terminator;
+		
+		private T object;
+		
+		protected TemporaryObject(final long milliseconds) {
+			this.milliseconds = milliseconds;
+		}
+		
+		public final long getMilliseconds() {
+			return this.milliseconds;
+		}
+		
+		public final synchronized T get() {
+			this.lastAccess = System.currentTimeMillis();
+			
+			if (this.object == null) {
+				this.object = this.newObject();
+				this.terminator = new Timer();
+				this.terminator.schedule(new TimerTask() {
+					
+					@Override
+					public final void run() {
+						TemporaryObject.this.update();
+					}
+					
+				}, this.getMilliseconds(), this.getMilliseconds());
+			}
+			
+			return this.object;
+		}
+		
+		final synchronized void update() {
+			if (this.getMilliseconds() <= System.currentTimeMillis() - this.lastAccess) {
+				this.deleteObject(this.object);
+				this.object = null;
+				
+				this.terminator.cancel();
+				this.terminator = null;
+			}
+		}
+		
+		protected abstract T newObject();
+		
+		protected void deleteObject(final T object) {
+			ignore(object);
+		}
+		
+		private static final long serialVersionUID = -2821058824513633614L;
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-02-14)
+	 */
+	public static final class Convolutions2D implements Serializable {
+		
+		private final CLContext context;
+		
+		private final CLKernel kernel;
+		
+		private int inputWidth, inputHeight, stride, convolutionCount;
+		
+		private long outputLength;
+		
+		public Convolutions2D(final CLContext context) {
+			this.context = context;
+			this.kernel = context.createAndBuildProgram(PROGRAM_SOURCE)
+					.createKernel("convolution2D");
+		}
+		
+		public final CLContext getContext() {
+			return this.context;
+		}
+		
+		public final CLKernel getKernel() {
+			return this.kernel;
+		}
+		
+		public final int getInputWidth() {
+			return this.inputWidth;
+		}
+		
+		public final int getInputHeight() {
+			return this.inputHeight;
+		}
+		
+		public final int getStride() {
+			return this.stride;
+		}
+		
+		public final int getConvolutionCount() {
+			return this.convolutionCount;
+		}
+		
+		public final long getOutputLength() {
+			return this.outputLength;
+		}
+		
+		public final Convolutions2D setInput(final float... input) {
+			this.getKernel().setArg(0, this.getContext().createInputBuffer(input));
+			
+			return this;
+		}
+		
+		public final Convolutions2D setInputDimensions(final int channelCount, final int inputWidth, final int inputHeight) {
+			this.getKernel().setArg(1, this.getContext().createInputBuffer(channelCount, inputWidth, inputHeight));
+			
+			this.inputWidth = inputWidth;
+			this.inputHeight = inputHeight;
+			
+			return this;
+		}
+		
+		public final Convolutions2D setStepping(final int sparsity, final int stride) {
+			this.getKernel().setArg(2, this.getContext().createInputBuffer(sparsity, stride));
+			
+			this.stride = stride;
+			
+			return this;
+		}
+		
+		public final Convolutions2D setConvolutions(final float... convolutions) {
+			this.getKernel().setArg(3, this.getContext().createInputBuffer(convolutions));
+			
+			return this;
+		}
+		
+		public final Convolutions2D setConvolutionsDimensions(final int convolutionCount, final int convolutionWidth, final int convolutionHeight) {
+			this.getKernel().setArg(4, this.getContext().createInputBuffer(convolutionCount, convolutionWidth, convolutionHeight));
+			
+			this.convolutionCount = convolutionCount;
+			
+			return this;
+		}
+		
+		public final Convolutions2D setOutput() {
+			this.outputLength = (long) this.getOutputWidth() * this.getOutputHeight() * this.getConvolutionCount();
+			
+			this.getKernel().setArg(5, this.getContext().createOutputBuffer(Sizeof.cl_float, this.getOutputLength()));
+			
+			return this;
+		}
+		
+		public final int getOutputWidth() {
+			return this.getInputWidth() / this.getStride();
+		}
+		
+		public final int getOutputHeight() {
+			return this.getInputHeight() / this.getStride();
+		}
+		
+		public final float[] compute() {
+			final float[] result = new float[(int) this.getOutputLength()];
+			
+			this.compute(result);
+			
+			return result;
+		}
+		
+		public final Convolutions2D compute(final float[] output) {
+			getKernel().enqueueNDRange(this.getOutputLength() / this.getConvolutionCount());
+			
+			if (output != null) {
+				this.getKernel().enqueueReadArg(5, output);
+			}
+			
+			return this;
+		}
+		
+		private static final long serialVersionUID = -8143736361978173371L;
+		
+	    public static final String PROGRAM_SOURCE =
+	            "__kernel void "
+	            + "convolution2D(__global const float * input,"
+	            + "             __global const int * inputDimensions,"
+	            + "             __global const int * stepping,"
+	            + "             __global const float * convolutions,"
+	            + "             __global const int * convolutionsDimensions,"
+	            + "             __global float * output)"
+	            + "{\n"
+	            + "	const int gid = get_global_id(0);\n"
+	            + "	const int inputChannels = inputDimensions[0];\n"
+	            + "	const int inputWidth = inputDimensions[1];\n"
+	            + "	const int inputHeight = inputDimensions[2];\n"
+	            + "	const int inputPlaneSize = inputWidth * inputHeight;\n"
+	            + "	const int sparsity = stepping[0];\n"
+	            + "	const int stride = stepping[1];\n"
+	            + "	const int convolutionCount = convolutionsDimensions[0];\n"
+	            + "	const int convolutionWidth = convolutionsDimensions[1];\n"
+	            + "	const int convolutionHeight = convolutionsDimensions[2];\n"
+	            + "	const int convolutionSize = convolutionWidth * convolutionHeight * inputChannels;\n"
+	            + "	const int x = stride * (gid % (inputWidth / stride));\n"
+	            + "	const int y = stride * (gid / (inputWidth / stride));\n"
+	            + "	const int virtualLeft = x + 1 - (convolutionWidth / 2);\n"
+	            + "	const int virtualRight = virtualLeft + convolutionWidth;\n"
+	            + "	const int virtualTop = y + 1 - (convolutionHeight / 2);\n"
+	            + "	const int virtualBottom = virtualTop + convolutionHeight;\n"
+	            + "	const int left = max(0, virtualLeft);\n"
+	            + "	const int right = min(virtualRight, inputWidth) - 1;\n"
+	            + "	const int top = max(0, virtualTop);\n"
+	            + "	const int bottom = min(virtualBottom, inputHeight) - 1;\n"
+	            + "	const int cleft = left - virtualLeft;\n"
+	            + "	const int cright = (virtualRight - 1) - right;\n"
+	            + "	const int ctop = top - virtualTop;\n"
+	            + "	const int cbottom = (virtualBottom - 1) - bottom;\n"
+	            + "	const int outputPlaneSize = (inputWidth / stride) * (inputHeight / stride);\n"
+	            + "	for (int o = 0; o < convolutionCount; ++o)\n"
+	            + "	{\n"
+	            + "		output[outputPlaneSize * o + gid] = 0;\n"
+	            + "	}\n"
+	            + "	for (int yy = top, cy = ctop; yy <= bottom; yy += sparsity, ++cy)\n"
+	            + "	{\n"
+	            + "		for (int xx = left, cx = cleft; xx <= right; xx += sparsity, ++cx)\n"
+	            + "		{\n"
+	            + "			const int inputOffset = (yy * inputWidth + xx);\n"
+	            + "			const int convolutionOffset = (cy * convolutionWidth + cx) * inputChannels;\n"
+	            + "			for (int c = 0; c < inputChannels; ++c)\n"
+	            + "			{\n"
+	            + "				const float inputValue = input[inputPlaneSize * c + inputOffset];\n"
+	            + "				for (int o = 0; o < convolutionCount; ++o)\n"
+	            + "				{\n"
+	            + "					output[outputPlaneSize * o + gid] += inputValue * convolutions[o * convolutionSize + convolutionOffset + c];\n"
+	            + "				}\n"
+	            + "			}\n"
+	            + "		}\n"
+	            + "	}\n"
+	            + "}";
 		
 	}
 	
