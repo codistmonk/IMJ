ff1be713ecf407c25e82fabfa82e4175e50c2269
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-03-04 09:56:46 +0000

    [IMJ][imj3][tools] Added Multifile2JPG.

---
+++ trunk/IMJ/src/imj3/tools/Multifile2JPG.java
@@ -0,0 +1,75 @@
+package imj3.tools;
+
+import static net.sourceforge.aprog.tools.Tools.baseName;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+import imj2.draft.AutoCloseableImageWriter;
+
+import imj3.core.Image2D;
+
+import net.sourceforge.aprog.tools.Canvas;
+import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
+import net.sourceforge.aprog.tools.IllegalInstantiationException;
+import net.sourceforge.aprog.tools.TicToc;
+import net.sourceforge.aprog.tools.Tools;
+
+/**
+ * @author codistmonk (creation 2015-03-04)
+ */
+public final class Multifile2JPG {
+	
+	private Multifile2JPG() {
+		throw new IllegalInstantiationException();
+	}
+	
+	/**
+	 * @param commandLineArguments
+	 * <br>Must not be null
+	 * @throws Exception 
+	 */
+	public static final void main(final String[] commandLineArguments) {
+		final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser(commandLineArguments);
+		final String filePath = arguments.get("file", "");
+		final int[] lods = arguments.get("lod", 4);
+		
+		for (final int lod : lods) {
+			final TicToc timer = new TicToc();
+			
+			Tools.debugPrint(new Date(timer.tic()));
+			Tools.debugPrint("Processing", filePath, "at LOD", lod);
+			
+			final Image2D image = new MultifileImage2D(new MultifileSource(filePath), lod);
+			final int width = image.getWidth();
+			final int height = image.getHeight();
+			final int optimalTileWidth = image.getOptimalTileWidth();
+			final int optimalTileHeight = image.getOptimalTileHeight();
+			final Canvas canvas = new Canvas().setFormat(width, height, BufferedImage.TYPE_3BYTE_BGR);
+			
+			for (int tileY = 0; tileY < height; tileY += optimalTileHeight) {
+				for (int tileX = 0; tileX < width; tileX += optimalTileWidth) {
+					canvas.getGraphics().drawImage((Image) image.getTile(tileX, tileY).toAwt(), tileX, tileY, null);
+				}
+			}
+			
+			final String format = "jpg";
+			final String outputPath = baseName(filePath) + "_lod" + lod + "." + format;
+			final float compressionQuality = 0.95F;
+			
+			Tools.debugPrint("Writing", outputPath);
+			
+			try (final AutoCloseableImageWriter imageWriter = new AutoCloseableImageWriter(format)
+					.setCompressionQuality(compressionQuality).setOutput(new FileOutputStream(outputPath))) {
+				imageWriter.write(canvas.getImage());
+			} catch (final Exception exception) {
+				exception.printStackTrace();
+			}
+			
+			Tools.debugPrint("Processing done in", timer.toc(), "ms");
+		}
+	}
+	
+}
