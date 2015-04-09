cd853c7808148d3a00137c2b380fbe6e448856fd
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-04-09 05:32:29 +0000

    [IMJ][imj3][draft] Updated CaffeinatedAnalysis.

--- trunk/IMJ/src/imj3/draft/CaffeinatedAnalysis.java
+++ trunk/IMJ/src/imj3/draft/CaffeinatedAnalysis.java
@@ -33,6 +33,7 @@ import imj3.tools.Image2DComponent;
 import imj3.tools.Image2DComponent.TileOverlay;
 
 import net.sourceforge.aprog.swing.SwingTools;
+import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
 import net.sourceforge.aprog.tools.Launcher;
 
@@ -49,10 +50,12 @@ public final class CaffeinatedAnalysis {
 	
 	/**
 	 * @param commandLineArguments
-	 * <br>Unused
+	 * <br>Must not be null
 	 */
 	public static final void main(final String[] commandLineArguments) {
-		final String imagePath = preferences.get("image.path", "");
+		final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser(commandLineArguments);
+		final String imagePath = arguments.get("file", preferences.get("image.path", ""));
+		debugPrint(imagePath);
 		final Image2D image;
 		
 		if (imagePath.isEmpty()) {
@@ -83,9 +86,6 @@ public final class CaffeinatedAnalysis {
 			
 		});
 		
-		final String userHome = System.getProperty("user.home");
-		final String caffe = userHome + "/workspace/caffe-master";
-		
 		mainPanel.add(setup(new Image2DComponent(image)), BorderLayout.CENTER);
 		mainPanel.setFocusable(true);
 		mainPanel.addKeyListener(new KeyAdapter() {
@@ -99,91 +99,7 @@ public final class CaffeinatedAnalysis {
 				}
 				
 				if (event.getKeyCode() == KeyEvent.VK_ENTER) {
-					final byte[] buffer = new byte[3073];
-					final File dataFile = new File("/dev/shm/imj3.data.bin");
-					final File labelsFile = new File("/dev/shm/imj3.labels.bin");
-					final Image2D image = component.getImage();
-					final Channels channels = image.getChannels();
-					final int imageWidth = image.getWidth();
-					final int imageHeight = image.getHeight();
-					final int patchSize = 32;
-					final int patchStride = 8;
-					long n = 0;
-					
-					try {
-						try (final OutputStream output = new FileOutputStream(dataFile)) {
-							debugPrint("Writing", dataFile);
-							for (int y = 0; y < imageHeight; y += patchStride) {
-								final int top = y - patchSize / 2;
-								final int bottom = top + patchSize;
-								
-								for (int x = 0; x < imageWidth; x += patchStride) {
-									final int left = x - patchSize / 2;
-									final int right = left + patchSize;
-									
-									for (int yy = top, i = 1; yy < bottom; ++yy) {
-										for (int xx = left; xx < right; ++xx, i += 3) {
-											if (0 <= yy && yy < imageHeight && 0 <= xx && xx < imageWidth) {
-												final long pixelValue = image.getPixelValue(xx, yy);
-												
-												for (int j = 0; j < 3; ++j) {
-													buffer[i + j] = (byte) channels.getChannelValue(pixelValue, j);
-												}
-											} else {
-												for (int j = 0; j < 3; ++j) {
-													buffer[i + j] = 0;
-												}
-											}
-										}
-									}
-									
-									output.write(buffer);
-									++n;
-								}
-							}
-							
-							output.close();
-						}
-						
-						debugPrint(n);
-						
-						final Process process = Runtime.getRuntime().exec(
-								array("Debug/RunCaffe", "test.prototxt", "test.caffemodel", dataFile.getPath(), labelsFile.getPath()),
-								array("LD_LIBRARY_PATH=$LD_LIBRARY_PATH:" + caffe + "/lib:" + caffe + "/build/lib", "GLOG_logtostderr=1"),
-								new File("../RunCaffe"));
-						
-						try (final OutputStream processControl = process.getOutputStream()) {
-							Launcher.redirectOutputsToConsole(process);
-							
-							processControl.close();
-							
-							debugPrint(process.waitFor());
-						}
-						
-						final BufferedImage result = new BufferedImage((imageWidth + patchStride - 1) / patchStride, (imageHeight + patchStride - 1) / patchStride, BufferedImage.TYPE_BYTE_BINARY);
-						final byte[] labelsBuffer = new byte[result.getWidth() * result.getHeight()];
-						
-						debugPrint(labelsBuffer.length);
-						
-						try (final InputStream input = new FileInputStream(labelsFile)) {
-							input.read(labelsBuffer);
-						}
-						
-						for (int y = 0, p = 0; y < result.getHeight(); ++y) {
-							for (int x = 0; x < result.getWidth(); ++x, ++p) {
-								result.setRGB(x, y, labelsBuffer[p] != 0 ? ~0 : 0);
-							}
-						}
-						
-						SwingTools.show(result, "result", false);
-					} catch (final IOException exception) {
-						throw new UncheckedIOException(exception);
-					} catch (final Exception exception) {
-						throw unchecked(exception);
-					} finally {
-						dataFile.delete();
-						labelsFile.delete();
-					}
+					process(component.getImage());
 				}
 			}
 			
@@ -207,6 +123,103 @@ public final class CaffeinatedAnalysis {
 		});
 		
 		return result;
+	}
+	
+	public static final void process(final Image2D image) {
+		final String userHome = System.getProperty("user.home");
+		final String caffe = userHome + "/workspace/caffe-master";
+		final byte[] buffer = new byte[3073];
+		final File dataFile = new File("/dev/shm/imj3.data.bin");
+		final File labelsFile = new File("/dev/shm/imj3.labels.bin");
+		final Channels channels = image.getChannels();
+		final int imageWidth = image.getWidth();
+		final int imageHeight = image.getHeight();
+		final int patchSize = 32;
+		final int patchStride = 8;
+		final int planeSize = patchSize * patchSize;
+		long n = 0;
+		
+		try {
+			try (final OutputStream output = new FileOutputStream(dataFile)) {
+				debugPrint("Writing", dataFile);
+				for (int y = 0; y < imageHeight; y += patchStride) {
+					final int top = y - patchSize / 2;
+					final int bottom = top + patchSize;
+					
+					for (int x = 0; x < imageWidth; x += patchStride) {
+						final int left = x - patchSize / 2;
+						final int right = left + patchSize;
+						
+						for (int yy = top; yy < bottom; ++yy) {
+							for (int xx = left; xx < right; ++xx) {
+								final int i = 1 + (yy - top) * patchSize + (xx - left);
+								if (0 <= yy && yy < imageHeight && 0 <= xx && xx < imageWidth) {
+									final long pixelValue = image.getPixelValue(xx, yy);
+									
+									for (int channelIndex = 0; channelIndex < 3; ++channelIndex) {
+										buffer[i + planeSize * channelIndex] = (byte) channels.getChannelValue(pixelValue, channelIndex);
+									}
+								} else {
+									for (int channelIndex = 0; channelIndex < 3; ++channelIndex) {
+										buffer[i + planeSize * channelIndex] = 0;
+									}
+								}
+							}
+						}
+						
+						output.write(buffer);
+						++n;
+					}
+				}
+				
+				output.close();
+			}
+			
+			debugPrint(n);
+			
+//			GroundTruth2Bin.BinView.main(dataFile.getPath());
+			
+			if (true) {
+				final Process process = Runtime.getRuntime().exec(
+						array("Debug/RunCaffe", "test.prototxt", "test.caffemodel", dataFile.getPath(), labelsFile.getPath()),
+						array("LD_LIBRARY_PATH=$LD_LIBRARY_PATH:" + caffe + "/lib:" + caffe + "/build/lib", "GLOG_logtostderr=1"),
+						new File("../RunCaffe"));
+				
+				try (final OutputStream processControl = process.getOutputStream()) {
+					Launcher.redirectOutputsToConsole(process);
+					
+					processControl.close();
+					
+					debugPrint(process.waitFor());
+				}
+				
+				final BufferedImage result = new BufferedImage(
+						(imageWidth + patchStride - 1) / patchStride,
+						(imageHeight + patchStride - 1) / patchStride, BufferedImage.TYPE_BYTE_BINARY);
+				final byte[] labelsBuffer = new byte[result.getWidth() * result.getHeight()];
+				
+				debugPrint(labelsBuffer.length);
+				
+				try (final InputStream input = new FileInputStream(labelsFile)) {
+					input.read(labelsBuffer);
+				}
+				
+				for (int y = 0, p = 0; y < result.getHeight(); ++y) {
+					for (int x = 0; x < result.getWidth(); ++x, ++p) {
+						result.setRGB(x, y, labelsBuffer[p] != 0 ? ~0 : 0);
+					}
+				}
+				
+				SwingTools.show(result, "result", false);
+			}
+		} catch (final IOException exception) {
+			throw new UncheckedIOException(exception);
+		} catch (final Exception exception) {
+			throw unchecked(exception);
+		} finally {
+			dataFile.delete();
+			labelsFile.delete();
+		}
 	}
 	
 }
