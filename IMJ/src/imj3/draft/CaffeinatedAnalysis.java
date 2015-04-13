524bd8cd5f74151816f0523101a40c3234917b65
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-04-13 08:11:28 +0000

    [IMJ][imj3][draft] Updated CaffeinatedAnalysis.

--- IMJ/src/imj3/draft/CaffeinatedAnalysis.java
+++ IMJ/src/imj3/draft/CaffeinatedAnalysis.java
@@ -204,10 +204,10 @@ public final class CaffeinatedAnalysis {
 		try {
 			try (final OutputStream output = new FileOutputStream(dataFile)) {
 				debugPrint("Writing", dataFile);
-				for (int y = 0; y < imageHeight; y += patchStride) {
+				for (int y = patchSize / 2; y < imageHeight; y += patchStride) {
 					final int top = y - patchSize / 2;
 					final int bottom = top + patchSize;
-					for (int x = 0; x < imageWidth; x += patchStride) {
+					for (int x = patchSize / 2; x < imageWidth; x += patchStride) {
 						final int left = x - patchSize / 2;
 						final int right = left + patchSize;
 						
@@ -255,8 +255,8 @@ public final class CaffeinatedAnalysis {
 			}
 			
 			final BufferedImage result = new BufferedImage(
-					(imageWidth + patchStride - 1) / patchStride,
-					(imageHeight + patchStride - 1) / patchStride, BufferedImage.TYPE_BYTE_BINARY);
+					(imageWidth - patchSize / 2 + patchStride - 1) / patchStride,
+					(imageHeight - patchSize / 2 + patchStride - 1) / patchStride, BufferedImage.TYPE_BYTE_BINARY);
 			final byte[] labelsBuffer = new byte[result.getWidth() * result.getHeight()];
 			
 			debugPrint(labelsBuffer.length);
