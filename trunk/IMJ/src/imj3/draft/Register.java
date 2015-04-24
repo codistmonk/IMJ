87f75a95cae71e3d163da0208f4391bef4cbeb8e
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-04-24 19:18:05 +0000

    [IMJ][imj3][draft] Updated Register.

--- trunk/IMJ/src/imj3/draft/Register.java
+++ trunk/IMJ/src/imj3/draft/Register.java
@@ -1,11 +1,15 @@
 package imj3.draft;
 
+import static java.lang.Math.log;
+import static net.sourceforge.aprog.swing.SwingTools.show;
 import static net.sourceforge.aprog.tools.MathTools.square;
 import static net.sourceforge.aprog.tools.Tools.*;
 import imj3.core.Channels;
 import imj3.core.Image2D;
 import imj3.core.Image2D.Pixel2DProcessor;
 import imj3.tools.IMJTools;
+import imj3.tools.Image2DComponent;
+import imj3.tools.PackedImage2D;
 
 import java.awt.geom.Point2D;
 import java.io.Serializable;
@@ -43,6 +47,10 @@ public final class Register {
 		final WarpField warpField = new WarpField(100, 100);
 		
 		debugPrint("score:", warpField.score(source, target));
+		
+		show(new Image2DComponent(source), "source", false);
+		show(new Image2DComponent(target), "target", false);
+		show(new Image2DComponent(warpField.warp(source, target, null)), "warped", false);
 	}
 	
 	/**
@@ -119,6 +127,27 @@ public final class Register {
 			});
 			
 			return result[0];
+		}
+		
+		public final Image2D warp(final Image2D source, final Image2D target, final Image2D result) {
+			final Image2D actualResult = result != null ? result : new PackedImage2D(
+					baseName(source.getId()) + "_warped_lod" + (int) (-log(target.getScale()) / log(2.0)),
+					target.getWidth(), target.getHeight(), source.getChannels());
+			
+			target.forEachPixel(new Pixel2DProcessor() {
+				
+				@Override
+				public final boolean pixel(final int x, final int y) {
+					actualResult.setPixelValue(x, y, WarpField.this.warp(source, target, x, y));
+					
+					return true;
+				}
+				
+				private static final long serialVersionUID = 4284446331825700908L;
+				
+			});
+			
+			return actualResult;
 		}
 		
 		private static final long serialVersionUID = -9073706319287807440L;
