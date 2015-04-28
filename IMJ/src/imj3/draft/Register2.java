80338f5d091a3d57439bfa64db9dfc9ac342ac8d
codistmonk <codistmonk@4ab19d5a-ac58-4ee5-a407-2e49e580f151>
2015-04-28 10:22:30 +0000

    [IMJ][imj3][draft] Updated Register2.

--- IMJ/src/imj3/draft/Register2.java
+++ IMJ/src/imj3/draft/Register2.java
@@ -3,6 +3,7 @@ package imj3.draft;
 import static imj3.draft.Register.add;
 import static imj3.draft.Register.lerp;
 import static imj3.draft.Register.newPatchOffsets;
+import static java.lang.Integer.decode;
 import static java.lang.Math.*;
 import static java.util.Arrays.stream;
 import static net.sourceforge.aprog.swing.SwingTools.show;
@@ -16,17 +17,32 @@ import imj3.tools.IMJTools;
 import imj3.tools.Image2DComponent;
 import imj3.tools.Image2DComponent.TileOverlay;
 
+
+
+
+
+
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+
+
+
+
+
 
 import net.sourceforge.aprog.tools.CommandLineArgumentsParser;
 import net.sourceforge.aprog.tools.ConsoleMonitor;
@@ -129,10 +145,53 @@ public final class Register2 {
 			debugPrint(i);
 			monitor.ping(i + "/" + iterations + "\r");
 			
-			focus(grid, image, windowSize - i * (windowSize - 1) / iterations, patchSize);
+			try {
+				focus(grid, image, windowSize - i * (windowSize - 1) / iterations, patchSize);
+			} catch (final RuntimeException exception) {
+				final String[] xy = exception.getMessage().split(",");
+				final Point2D point = grid.get(decode(xy[0]), decode(xy[1]));
+				final int w = 600;
+				final int h = w;
+				
+				showGrid(grid, w, h, "grid " + i).setRGB((int) (point.getX() * w), (int) (point.getY() * h), 0xFFFF0000);
+				
+				exception.printStackTrace();
+				
+				break;
+			}
 		}
 		
 		monitor.pause();
+	}
+	
+	public static final BufferedImage showGrid(final ParticleGrid grid, final int width, final int height, final String title) {
+		final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		final Graphics2D graphics = image.createGraphics();
+		final Polygon polygon = new Polygon(new int[4], new int[4], 4);
+		
+		grid.forEach((x, y) -> {
+			if (0 < x && 0 < y) {
+				setPoint(polygon, 0, grid.get(x - 1, y - 1), width, height);
+				setPoint(polygon, 1, grid.get(x - 1, y), width, height);
+				setPoint(polygon, 2, grid.get(x, y), width, height);
+				setPoint(polygon, 3, grid.get(x, y - 1), width, height);
+			}
+			
+			graphics.draw(polygon);
+			
+			return true;
+		});
+		
+		graphics.dispose();
+		
+		show(image, title, false);
+		
+		return image;
+	}
+	
+	public static final void setPoint(final Polygon polygon, final int index, final Point2D normalizedPoint, final int width, final int height) {
+		polygon.xpoints[index] = (int) (normalizedPoint.getX() * width);
+		polygon.ypoints[index] = (int) (normalizedPoint.getY() * height);
 	}
 	
 	public static final void overlay(final ParticleGrid grid, final Image2DComponent component) {
@@ -264,6 +323,8 @@ public final class Register2 {
 		final int imageWidth = image.getWidth();
 		final int imageHeight = image.getHeight();
 		final Point2D[] patchOffsets = newPatchOffsets(windowSize);
+//		final Polygon polygon = new Polygon(new int[4], new int[4], 4);
+		final Polygon polygon = new Polygon(new int[8], new int[8], 8);
 		
 		grid.forEach((x, y) -> {
 			final Point2D normalizedPixel = grid.get(x, y);
@@ -281,29 +342,178 @@ public final class Register2 {
 				}
 			}
 			
-			if (0 < x && x + 1 < grid.getWidth() && 0 < y && y + 1 < grid.getHeight()) {
-				for (int i = 0; i <= 3; ++i) {
-					final int i0 = i & 1;
-					final int i1 = (i >> 1) & 1;
-					final int dx = i1 - i0;
-					final int dy = i0 + i1 - 1;
-					
-					move(grid, x + dx, y + dy, bestOffset, imageWidth, imageHeight);
-				}
-			}
-			
-			if (0 < x && x + 1 < grid.getWidth() && 0 < y && y + 1 < grid.getHeight()) {
-				checkInducedTopology(grid, x, y, imageWidth, imageHeight);
-			}
+			if (bestOffset != null) {
+				updateSurroundingPolygon8(grid, x, y, imageWidth, imageHeight, polygon);
+				final Shape shape = new Path2D.Double(polygon);
 				
-			move(grid, x, y, bestOffset, imageWidth, imageHeight);
-			
-			if (0 < x && x + 1 < grid.getWidth() && 0 < y && y + 1 < grid.getHeight()) {
-				checkInducedTopology(grid, x, y, imageWidth, imageHeight);
+				if (!shape.contains(normalizedPixel.getX() * imageWidth, normalizedPixel.getY() * imageHeight)) {
+					debugError(x, y);
+					debugError(Arrays.toString(polygon.xpoints));
+					debugError(Arrays.toString(polygon.ypoints));
+					debugError((int) (normalizedPixel.getX() * imageWidth), (int) (normalizedPixel.getY() * imageHeight));
+					
+					throw new RuntimeException(x + "," + y);
+				}
+				
+				for (final int s : new int[] { 4, 8, 16, 32, -32 }) {
+					if (shape.contains(normalizedPixel.getX() * imageWidth + bestOffset.getX() / s / 2.0, normalizedPixel.getY() * imageHeight + bestOffset.getY() / s / 2.0)) {
+						normalizedPixel.setLocation(normalizedPixel.getX() + bestOffset.getX() / s / 2.0 / imageWidth, normalizedPixel.getY() + bestOffset.getY() / s / 2.0 / imageHeight);
+						break;
+					}
+				}
 			}
 			
 			return true;
 		});
+	}
+	
+	public static final void updateSurroundingPolygon4(final ParticleGrid grid, final int x, final int y, final int imageWidth, final int imageHeight, final Polygon result) {
+		final Point2D point = grid.get(x, y);
+		
+		if (0 < y) {
+			final Point2D neighbor = grid.get(x, y - 1);
+			result.xpoints[0] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[0] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x, y + 1);
+			result.xpoints[0] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[0] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+			
+		}
+		
+		if (0 < x) {
+			final Point2D neighbor = grid.get(x - 1, y);
+			result.xpoints[1] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[1] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x + 1, y);
+			result.xpoints[1] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[1] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+			
+		}
+		
+		if (y + 1 < grid.getHeight()) {
+			final Point2D neighbor = grid.get(x, y + 1);
+			result.xpoints[2] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[2] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x, y - 1);
+			result.xpoints[2] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[2] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+			
+		}
+		
+		if (x + 1 < grid.getWidth()) {
+			final Point2D neighbor = grid.get(x + 1, y);
+			result.xpoints[3] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[3] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x - 1, y);
+			result.xpoints[3] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[3] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+	}
+	
+	public static final void updateSurroundingPolygon8(final ParticleGrid grid, final int x, final int y, final int imageWidth, final int imageHeight, final Polygon result) {
+		final int gridWidth = grid.getWidth();
+		final int gridHeight = grid.getHeight();
+		final Point2D point = grid.get(x, y);
+		int i = 0;
+		
+		if (0 < y) {
+			final Point2D neighbor = grid.get(x, y - 1);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x, y + 1);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+		
+		++i;
+		
+		if (0 < y && 0 < x) {
+			final Point2D neighbor = grid.get(x - 1, y - 1);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x + 1, y + 1);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+		
+		++i;
+		
+		if (0 < x) {
+			final Point2D neighbor = grid.get(x - 1, y);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x + 1, y);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+		
+		++i;
+		
+		if (y + 1 < gridHeight && 0 < x) {
+			final Point2D neighbor = grid.get(x - 1, y + 1);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x + 1, y - 1);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+		
+		++i;
+		
+		if (y + 1 < gridHeight) {
+			final Point2D neighbor = grid.get(x, y + 1);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x, y - 1);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+			
+		}
+		
+		++i;
+		
+		if (y + 1 < gridHeight && x + 1 < gridWidth) {
+			final Point2D neighbor = grid.get(x + 1, y + 1);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x - 1, y - 1);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+		
+		++i;
+		
+		if (x + 1 < gridWidth) {
+			final Point2D neighbor = grid.get(x + 1, y);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x - 1, y);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
+		
+		++i;
+		
+		if (0 < y && x + 1 < gridWidth) {
+			final Point2D neighbor = grid.get(x + 1, y - 1);
+			result.xpoints[i] = (int) (neighbor.getX() * imageWidth);
+			result.ypoints[i] = (int) (neighbor.getY() * imageHeight);
+		} else {
+			final Point2D neighbor = grid.get(x - 1, y + 1);
+			result.xpoints[i] = (int) ((2.0 * point.getX() - neighbor.getX()) * imageWidth);
+			result.ypoints[i] = (int) ((2.0 * point.getY() - neighbor.getY()) * imageHeight);
+		}
 	}
 	
 	public static final void move(final ParticleGrid grid, final int x, final int y,
@@ -312,38 +522,88 @@ public final class Register2 {
 			final double d = offset.distance(ZERO);
 			
 			if (0.0 < d) {
-				final double nx = offset.getX() / d;
-				final double ny = offset.getY() / d;
+				final Point2D normalizedOffset = new Point2D.Double(offset.getX() / imageWidth, offset.getY() / imageHeight);
 				final Point2D normalizedPixel = grid.get(x, y);
-				double closestNeighborDistance = Double.POSITIVE_INFINITY;
+				final Point2D tmp = new Point2D.Double();
+				double smallestK = 1.0;
 				
 				{
 					for (int i = 0; i <= 3; ++i) {
-						final int i0 = i & 1;
-						final int i1 = (i >> 1) & 1;
-						final int dx = i1 - i0;
-						final int dy = i0 + i1 - 1;
-						final Point2D neighbor = grid.get(x + dx, y + dy);
-//						final double neighborDistance = normalizedPixel.distance(neighbor);
-						final double neighborDistance = neighbor.getX() * nx + neighbor.getY() * ny;
+						final Point2D opposition = getInternalDirection(grid, x, y, i, tmp);
 						
-						if (0.0 < neighborDistance && neighborDistance < closestNeighborDistance) {
-							closestNeighborDistance = neighborDistance;
+						if (opposition != null) {
+							final double dotNormalizedOffsetOpposition = dot(normalizedOffset, opposition);
+							
+							if (dotNormalizedOffsetOpposition < 0.0) {
+								final Point2D neighbor = getNeighbor(grid, x, y, i);
+								
+								// (normalizedPixel + k * normalizedOffset) . opposition == neighbor . opposition
+								// <- normalizedPixel . opposition + k * normalizedOffset . opposition == neighbor . opposition
+								// <- k * normalizedOffset . opposition == neighbor . opposition - normalizedPixel . opposition
+								// <- k = (neighbor . opposition - normalizedPixel . opposition) / normalizedOffset . opposition
+								final double k = (dot(neighbor, opposition) - dot(normalizedPixel, opposition)) / dotNormalizedOffsetOpposition;
+								
+								if (k < smallestK) {
+									smallestK = k;
+								}
+							}
 						}
 					}
 				}
 				
-				final double k = min(d, closestNeighborDistance) / d / 8.0;
+				final double k = smallestK / 2.0;
 				
-				if (1.0 < k) {
-					debugError(k, d, closestNeighborDistance);
+				if (k < -1.0) {
+					debugError(k);
+					debugError(normalizedPixel, normalizedOffset);
+					throw new RuntimeException();
 				}
 				
 				normalizedPixel.setLocation(
-						normalizedPixel.getX() + k * offset.getX() / imageWidth,
-						normalizedPixel.getY() + k * offset.getY() / imageHeight);
+						normalizedPixel.getX() + k * normalizedOffset.getX(),
+						normalizedPixel.getY() + k * normalizedOffset.getY());
 			}
 		}
+	}
+	
+	private static final Point[] neighborOffsets = { new Point(0, -1), new Point(-1, 0), new Point(0, 1), new Point(1, 0) };
+	
+	public static final double dot(final Point2D p1, final Point2D p2) {
+		return p1.getX() * p2.getX() + p1.getY() * p2.getY();
+	}
+	
+	public static final Point2D getNeighbor(final ParticleGrid grid, final int x, final int y, final int neighborIndex) {
+		final Point neighborOffset = neighborOffsets[neighborIndex];
+		
+		return grid.get(x + neighborOffset.x, y + neighborOffset.y);
+	}
+	
+	public static final Point2D getInternalDirection(final ParticleGrid grid, final int x, final int y, final int neighborIndex, final Point2D result) {
+		final int gridWidth = grid.getWidth();
+		final int gridHeight = grid.getHeight();
+		final Point neighbor0Offset = neighborOffsets[neighborIndex];
+		final int neighbor0X = x + neighbor0Offset.x;
+		final int neighbor0Y = y + neighbor0Offset.y;
+		
+		if (neighbor0X < 0 || gridWidth <= neighbor0X || neighbor0Y < 0 || gridHeight <= neighbor0Y) {
+			return null;
+		}
+		
+		final Point neighbor1Offset = neighborOffsets[(neighborIndex + 1) % neighborOffsets.length];
+		final int neighbor1X = x + neighbor1Offset.x;
+		final int neighbor1Y = y + neighbor1Offset.y;
+		
+		if (neighbor1X < 0 || gridWidth <= neighbor1X || neighbor1Y < 0 || gridHeight <= neighbor1Y) {
+			return null;
+		}
+		
+		final Point2D neighbor0 = grid.get(neighbor0X, neighbor0Y);
+		final Point2D neighbor1 = grid.get(neighbor1X, neighbor1Y);
+		
+		// left-handed coordinates
+		result.setLocation(neighbor1.getY() - neighbor0.getY(), -(neighbor1.getX() - neighbor0.getX()));
+		
+		return result;
 	}
 	
 	public static void checkInducedTopology(final ParticleGrid grid, final int x, final int y,
@@ -351,30 +611,21 @@ public final class Register2 {
 		final Point2D normalizedPixel = grid.get(x, y);
 		
 		for (int i = 0; i <= 3; ++i) {
-			final int i0 = i & 1;
-			final int i1 = (i >> 1) & 1;
-			final int dx = i1 - i0;
-			final int dy = i0 + i1 - 1;
-			final Point2D neighbor = grid.get(x + dx, y + dy);
+			final Point neighborOffset = neighborOffsets[i];
+			final Point2D neighbor = grid.get(x + neighborOffset.x, y + neighborOffset.y);
 			
 			xs[i] = (int) (neighbor.getX() * imageWidth);
 			ys[i] = (int) (neighbor.getY() * imageHeight);
 		}
 		
-		swap(xs, 2, 3);
-		swap(ys, 2, 3);
 		final Polygon polygon = new Polygon(xs, ys, xs.length);
 		
-		if (!polygon.contains(normalizedPixel.getX() * imageWidth, normalizedPixel.getY() * imageHeight)) {
-			debugError(Arrays.toString(xs));
-			debugError(Arrays.toString(ys));
-			debugError(normalizedPixel, (int) (normalizedPixel.getX() * imageWidth), (int) (normalizedPixel.getY() * imageHeight));
-			debugError();
-			debugError(grid.get(x, y - 1));
-			debugError(grid.get(x - 1, y));
-			debugError(grid.get(x, y + 1));
-			debugError(grid.get(x + 1, y));
-			throw new IllegalStateException();
+		if (!polygon.contains((int) (normalizedPixel.getX() * imageWidth), (int) (normalizedPixel.getY() * imageHeight))) {
+//			debugError();
+//			debugError(Arrays.toString(xs));
+//			debugError(Arrays.toString(ys));
+//			debugError(normalizedPixel, (int) (normalizedPixel.getX() * imageWidth), (int) (normalizedPixel.getY() * imageHeight));
+//			throw new IllegalStateException();
 		}
 	}
 	
