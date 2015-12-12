d27b0e293ce085290ce3d1bcbf570c3a7ec6619d
codistmonk <codistmonk@users.noreply.github.com>
2015-12-12 12:45:32 +0100

    [imj3][tools][PrintInfo] Handle annotations; add CSV export option.

--- src/imj3/draft/PrintInfo.java
+++ src/imj3/draft/PrintInfo.java
@@ -1,12 +1,18 @@
 package imj3.draft;
 
+import imj3.core.Image2D;
+import imj3.tools.IMJTools;
+
+import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import imj3.core.Image2D;
-import imj3.tools.IMJTools;
 import multij.tools.CommandLineArgumentsParser;
 import multij.tools.IllegalInstantiationException;
+import multij.tools.Tools;
+import multij.xml.XMLTools;
+
+import org.w3c.dom.Document;
 
 /**
  * @author codistmonk (creation 2015-07-29)
@@ -24,22 +30,47 @@ public final class PrintInfo {
 	public static final void main(final String[] commandLineArguments) {
 		final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser(commandLineArguments);
 		final String imagePath = arguments.get("image", "");
-		final Image2D image = IMJTools.read(imagePath);
+		final String svgPath = arguments.get("svg", "");
 		final String key = arguments.get("get", "");
-		final Map<Object, Object> info = new LinkedHashMap<>();
+		final String csv = arguments.get("csv", "");
 		
-		info.put("path", imagePath);
-		info.put("width", image.getWidth());
-		info.put("height", image.getHeight());
-		info.put("channels", image.getChannels());
-		info.put("optimalTileWidth", image.getOptimalTileWidth());
-		info.put("optimalTileHeight", image.getOptimalTileHeight());
+		if (!imagePath.isEmpty()) {
+			final Image2D image = IMJTools.read(imagePath);
+			final Map<Object, Object> info = new LinkedHashMap<>();
+			
+			info.put("path", imagePath);
+			info.put("width", image.getWidth());
+			info.put("height", image.getHeight());
+			info.put("channels", image.getChannels());
+			info.put("optimalTileWidth", image.getOptimalTileWidth());
+			info.put("optimalTileHeight", image.getOptimalTileHeight());
+			
+			print(info, csv, key);
+		}
 		
+		if (!svgPath.isEmpty()) {
+			final Document svg = SVGTools.readXML(new File(svgPath));
+			final Map<Object, Object> info = new LinkedHashMap<>();
+			
+			info.put("path", svgPath);
+			info.put("width", svg.getDocumentElement().getAttribute("width"));
+			info.put("height", svg.getDocumentElement().getAttribute("height"));
+			info.put("regions", XMLTools.getNodes(svg, "//*[@objectId]").size()); // XXX why is the namespace (imj:) not needed?
+			
+			print(info, csv, key);
+		}
+	}
+	
+	public static final void print(final Map<Object, Object> info, final String csv, final String key) {
 		if (key.isEmpty()) {
-			info.forEach((k, v) -> System.out.println(k + ": " + v));
+			if (csv.isEmpty()) {
+				info.forEach((k, v) -> System.out.println(k + ": " + v));
+			} else {
+				System.out.println(Tools.join(csv, info.values()));
+			}
 		} else {
 			System.out.println(info.get(key));
 		}
 	}
-
+	
 }
